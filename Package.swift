8e2c11678bc9aff88c339a3b2a259e83558fcf4b
Christopher Jr Riley <chrisjr@cjrriley.com>
2024-01-03 04:31:15 -0500

    Add supported platforms

--- Package.swift
+++ Package.swift
@@ -5,6 +5,13 @@ import PackageDescription
 
 let package = Package(
     name: "ATProtoKit",
+    platforms: [
+        .iOS(.v14),
+        .macOS(.v13),
+        .tvOS(.v14),
+        .visionOS(.v1),
+        .watchOS(.v7)
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
