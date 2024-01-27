25a28ef8dfbc1b1704de162d89784a723c837cb2
Christopher Jr Riley <chrisjr@cjrriley.com>
2024-01-27 01:18:34 -0500

    Add models for record embeds with media

---
+++ Sources/ATProtoKit/Models/Lexicons/app.bsky/Embed/BskyEmbedRecordWithMedia.swift
@@ -0,0 +1,74 @@
+//
+//  BskyEmbedRecordWithMedia.swift
+//
+//
+//  Created by Christopher Jr Riley on 2024-01-27.
+//
+
+import Foundation
+
+public struct EmbedRecordWithMedia: Codable {
+    public let record: EmbedRecord
+    public let media: MediaUnion
+}
+
+public struct EmbedRecordWithMediaView: Codable {
+    public let record: EmbedRecordView
+    public let media: MediaViewUnion
+}
+
+public enum MediaUnion: Codable {
+    case embedImages(EmbedImages)
+    case embedExternal(EmbedExternal)
+
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.singleValueContainer()
+
+        if let value = try? container.decode(EmbedImages.self) {
+            self = .embedImages(value)
+        } else if let value = try? container.decode(EmbedExternal.self) {
+            self = .embedExternal(value)
+        } else {
+            throw DecodingError.typeMismatch(PostUnion.self, DecodingError.Context(codingPath: decoder.codingPath, debugDescription: "Unknown MediaUnion type"))
+        }
+    }
+
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.singleValueContainer()
+
+        switch self {
+            case .embedImages(let media):
+                try container.encode(media)
+            case .embedExternal(let media):
+                try container.encode(media)
+        }
+    }
+}
+
+public enum MediaViewUnion: Codable {
+    case embedImagesView(EmbedImagesView)
+    case embedExternalView(EmbedExternalView)
+
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.singleValueContainer()
+
+        if let value = try? container.decode(EmbedImagesView.self) {
+            self = .embedImagesView(value)
+        } else if let value = try? container.decode(EmbedExternalView.self) {
+            self = .embedExternalView(value)
+        } else {
+            throw DecodingError.typeMismatch(PostUnion.self, DecodingError.Context(codingPath: decoder.codingPath, debugDescription: "Unknown MediaViewUnion type"))
+        }
+    }
+
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.singleValueContainer()
+
+        switch self {
+            case .embedImagesView(let mediaView):
+                try container.encode(mediaView)
+            case .embedExternalView(let mediaView):
+                try container.encode(mediaView)
+        }
+    }
+}
