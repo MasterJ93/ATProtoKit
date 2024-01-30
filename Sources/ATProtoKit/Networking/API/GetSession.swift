b4d1127dda14f885705e98329c71923116c3c8fc
Christopher Jr Riley <chrisjr@cjrriley.com>
2024-01-29 21:28:49 -0500

    Implement GetSession
    At this time, this method doesn't work. Will need to
    get back at this.

--- Sources/ATProtoKit/Networking/API/GetSession.swift
+++ Sources/ATProtoKit/Networking/API/GetSession.swift
@@ -1,8 +1,27 @@
 //
-//  File.swift
-//  
+//  GetSession.swift
+//
 //
 //  Created by Christopher Jr Riley on 2024-01-29.
 //
 
 import Foundation
+
+extension ATProtoKit {
+    // This doesn't work at the moment.
+    public func getSession() async throws -> Result<UserSession, Error> {
+        guard let pdsURL = session.pdsURL, let url = URL(string: "\(pdsURL)/xrpc/com.atproto.server.getSession") else {
+            return .failure(NSError(domain: "", code: -1, userInfo: [NSLocalizedDescriptionKey: "Invalid URL"]))
+        }
+
+        let request = APIClientService.createRequest(forRequest: url, andMethod: .get)
+
+        do {
+            let response = try await APIClientService.sendRequest(request, decodeTo: UserSession.self)
+
+            return .success(response)
+        } catch {
+            return .failure(NSError(domain: "", code: -1, userInfo: [NSLocalizedDescriptionKey : "Error: \(error)"]))
+        }
+    }
+}
