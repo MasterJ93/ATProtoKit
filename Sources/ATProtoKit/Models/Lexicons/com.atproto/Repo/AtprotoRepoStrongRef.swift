f6edddce0aa570dd2ccee2bc0e7711dbb44ffaaf
Christopher Jr Riley <chrisjr@cjrriley.com>
2024-02-02 20:03:41 -0500

    Add custom initializers for strong references

--- Sources/ATProtoKit/Models/Lexicons/com.atproto/Repo/AtprotoRepoStrongRef.swift
+++ Sources/ATProtoKit/Models/Lexicons/com.atproto/Repo/AtprotoRepoStrongRef.swift
@@ -11,6 +11,17 @@ public struct StrongReference: Codable {
     public let recordURI: String
     public let cidHash: String
 
+    public init(recordURI: String, cidHash: String) {
+        self.recordURI = recordURI
+        self.cidHash = cidHash
+    }
+
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.recordURI = try container.decode(String.self, forKey: .recordURI)
+        self.cidHash = try container.decode(String.self, forKey: .cidHash)
+    }
+
     enum CodingKeys: String, CodingKey {
         case recordURI = "uri"
         case cidHash = "cid"
