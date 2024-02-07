1d56bde0464e0ca9f9adcb40409c05ce3d379573
Christopher Jr Riley <chrisjr@cjrriley.com>
2024-02-06 22:27:18 -0500

    Add SwiftSoup

--- Package.swift
+++ Package.swift
@@ -18,11 +18,16 @@ let package = Package(
             name: "ATProtoKit",
             targets: ["ATProtoKit"]),
     ],
+    dependencies: [
+        .package(url: "https://github.com/scinfu/SwiftSoup.git", from: "2.7.0")
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "ATProtoKit"),
+            name: "ATProtoKit",
+          dependencies: ["SwiftSoup"]
+        ),
 //        .testTarget(
 //            name: "ATProtoKitTests",
 //            dependencies: ["ATProtoKit"]),
