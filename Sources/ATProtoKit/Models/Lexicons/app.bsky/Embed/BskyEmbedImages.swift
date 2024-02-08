86003c9774ca5cb96c58fa670b084ab5751de56a
Christopher Jr Riley <chrisjr@cjrriley.com>
2024-02-07 21:12:43 -0500

    Add custom inits
    This fixes an issue where Xcode's autocomplete fails
    to see the initalizer.

--- Sources/ATProtoKit/Models/Lexicons/app.bsky/Embed/BskyEmbedImages.swift
+++ Sources/ATProtoKit/Models/Lexicons/app.bsky/Embed/BskyEmbedImages.swift
@@ -11,6 +11,10 @@ public struct EmbedImages: Codable {
     public let type: String = "app.bsky.embed.images"
     public let images: [EmbedImage]
 
+    public init(images: [EmbedImage]) {
+        self.images = images
+    }
+
     enum CodingKeys: String, CodingKey {
         case type = "$type"
         case images
@@ -21,15 +25,30 @@ public struct EmbedImage: Codable {
     public let image: UploadBlobOutput
     public let altText: String
     public let aspectRatio: AspectRatio?
+
+    public init(image: UploadBlobOutput, altText: String, aspectRatio: AspectRatio?) {
+        self.image = image
+        self.altText = altText
+        self.aspectRatio = aspectRatio
+    }
 }
 
 public struct AspectRatio: Codable {
     public let width: Int
     public let height: Int
+
+    public init(width: Int, height: Int) {
+        self.width = width
+        self.height = height
+    }
 }
 
 public struct EmbedImagesView: Codable {
     public let images: [ViewImage]
+
+    public init(images: [ViewImage]) {
+        self.images = images
+    }
 }
 
 public struct ViewImage: Codable {
@@ -37,4 +56,11 @@ public struct ViewImage: Codable {
     public let fullSize: String
     public let altText: String
     public let aspectRatio: AspectRatio?
+
+    public init(thumbnail: String, fullSize: String, altText: String, aspectRatio: AspectRatio?) {
+        self.thumbnail = thumbnail
+        self.fullSize = fullSize
+        self.altText = altText
+        self.aspectRatio = aspectRatio
+    }
 }
