0f77127b206d7bf0b2f6ef5837fc687a3f03c311
Christopher Jr Riley <chrisjr@cjrriley.com>
2024-02-10 12:36:41 -0500

    Add method for updating handles

---
+++ Sources/ATProtoKit/Networking/Generic/com.atproto/UpdateHandle.swift
@@ -0,0 +1,25 @@
+//
+//  UpdateHandle.swift
+//
+//
+//  Created by Christopher Jr Riley on 2024-02-10.
+//
+
+import Foundation
+
+extension ATProtoKit {
+    public func updateHandle(_ handle: UpdateHandleQuery) async throws {
+        guard let sessionURL = session.pdsURL,
+              let requestURL = URL(string: "\(sessionURL)/xrpc/com.atproto.identity.updateHandle") else {
+            throw NSError(domain: "", code: -1, userInfo: [NSLocalizedDescriptionKey: "Invalid URL"])
+        }
+
+        let request = APIClientService.createRequest(forRequest: requestURL, andMethod: .post, authorizationValue: "Bearer \(session.accessToken)")
+
+        do {
+            try await APIClientService.sendRequest(request, withEncodingBody: handle)
+        } catch {
+            throw NSError(domain: "", code: -1, userInfo: [NSLocalizedDescriptionKey: "\(error)"])
+        }
+    }
+}
