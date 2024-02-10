a4698c66bf60da6722985bfacd343b7fe800c4db
Christopher Jr Riley <chrisjr@cjrriley.com>
2024-02-10 14:08:54 -0500

    Re-create ATProtoError
    This should be a good starting point for now.

--- Sources/ATProtoKit/Errors/ATProtoError.swift
+++ Sources/ATProtoKit/Errors/ATProtoError.swift
@@ -7,33 +7,63 @@
 
 import Foundation
 
-public enum ATProtoError: Error {
-    case badRequest
-    case unauthorized
-    case forbidden
-    case notFound
-    case payloadTooLarge
-    case tooManyRequests
-    case internalServerError
-    case notImplemented
-    case badGateway
-    case serviceUnavailable
-    case gatewayTimeout
-    
-    init?(statusCode: Int) {
-        switch statusCode {
-            case 400: self = .badRequest
-            case 401: self = .unauthorized
-            case 403: self = .forbidden
-            case 404: self = .notFound
-            case 413: self = .payloadTooLarge
-            case 429: self = .tooManyRequests
-            case 500: self = .internalServerError
-            case 501: self = .notImplemented
-            case 502: self = .badGateway
-            case 503: self = .serviceUnavailable
-            case 504: self = .gatewayTimeout
-            default: return nil
+public enum ATProtoError: Error, Decodable {
+    case badRequest(message: String) // Error 400
+    case unauthorized(mesage: String) // Error 401
+    case forbidden(message: String) // Error 403
+    case notFound(message: String) // Error 404
+    case payloadTooLarge(message: String) // Error 413
+    case tooManyRequests(message: String) // Error 429
+    case internalServerError(message: String) // Error 500
+    case notImplemented(message: String) // Error 501
+    case badGateway(message: String) // Error 502
+    case serviceUnavailable(message: String) // Error 503
+    case gatewayTimeout(message: String) // Error 504
+    case unknown(message: String) // Unknown Error
+
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+
+        let errorType = try container.decode(String.self, forKey: .error)
+        let message = try container.decode(String.self, forKey: .message)
+
+        switch errorType {
+            case 
+                "InvalidRequest",
+                "ExpiredToken",
+                "InvalidToken",
+                "BlockedActor",
+                "BlockedByActor",
+                "UnknownFeed",
+                "UnknownList",
+                "NotFound",
+                "BadQueryString",
+                "SubjectHasAction",
+                "RecordNotFound",
+                "RepoNotFound",
+                "InvalidSwap",
+                "AccountNotFound",
+                "InvalidEmail",
+                "InvalidHandle",
+                "InvalidPassword",
+                "InvalidInviteCode",
+                "HandleNotAvailable",
+                "UnsupportedDomain",
+                "UnresolvableDid",
+                "IncompatibleDidDoc",
+                "AccountTakedown",
+                "DuplicateCreate",
+                "TokenRequired":
+                self = .badRequest(message: message)
+            case "InternalServerError":
+                self = .internalServerError(message: message)
+            default:
+                self = .unknown(message: message)
         }
+    }
+
+    enum CodingKeys: String, CodingKey {
+        case error
+        case message
     }
 }
