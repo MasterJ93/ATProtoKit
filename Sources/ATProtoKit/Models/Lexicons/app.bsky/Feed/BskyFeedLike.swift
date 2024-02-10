32316d83d3147239eef4d68a90baca26b75b44b1
Christopher Jr Riley <chrisjr@cjrriley.com>
2024-02-10 02:54:12 -0500

    Tweak LikeRecord model and CreateLike method
    - Changed the LikeRecord model name to FeedLike
    (Will need to re-think some of these names.)
    - Added a request body for the CreateLike method.

--- Sources/ATProtoKit/Models/Lexicons/app.bsky/Feed/BskyFeedLike.swift
+++ Sources/ATProtoKit/Models/Lexicons/app.bsky/Feed/BskyFeedLike.swift
@@ -7,20 +7,23 @@
 
 import Foundation
 
-public struct LikeRecord: Codable {
+public struct FeedLike: Codable {
     public let type: String = "app.bsky.feed.like"
-    public let subject: StrongReference
+    public let recordURI: String
+    public let cidHash: String
     @DateFormatting public var createdAt: Date
 
-    public init(subject: StrongReference, createdAt: Date) {
-        self.subject = subject
+    public init(recordURI: String, cidHash: String, createdAt: Date) {
+        self.recordURI = recordURI
+        self.cidHash = cidHash
         self._createdAt = DateFormatting(wrappedValue: createdAt)
     }
 
     public init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
 
-        self.subject = try container.decode(StrongReference.self, forKey: .subject)
+        self.recordURI = try container.decode(String.self, forKey: .recordURI)
+        self.cidHash = try container.decode(String.self, forKey: .cidHash)
         self.createdAt = try container.decode(DateFormatting.self, forKey: .createdAt).wrappedValue
     }
 
@@ -28,13 +31,15 @@ public struct LikeRecord: Codable {
         var container = encoder.container(keyedBy: CodingKeys.self)
 
         try container.encode(self.type, forKey: .type)
-        try container.encode(self.subject, forKey: .subject)
+        try container.encode(self.recordURI, forKey: .recordURI)
+        try container.encode(self.cidHash, forKey: .cidHash)
         try container.encode(self._createdAt, forKey: .createdAt)
     }
 
     enum CodingKeys: String, CodingKey {
         case type = "$type"
-        case subject
+        case recordURI = "uri"
+        case cidHash = "cid"
         case createdAt
     }
 }
--- Sources/ATProtoKit/Networking/API/com.atproto/CreateLike.swift
+++ Sources/ATProtoKit/Networking/API/com.atproto/CreateLike.swift
@@ -1,8 +1,36 @@
 //
-//  File.swift
-//  
+//  CreateLike.swift
+//
 //
 //  Created by Christopher Jr Riley on 2024-02-08.
 //
 
 import Foundation
+
+extension ATProtoKit {
+    public func createLikeRecord(_ strongReference: StrongReference, createdAt: Date = Date.now) async throws {
+        guard let sessionURL = session.pdsURL,
+              let requestURL = URL(string: "\(sessionURL)/xrpc/com.atproto.repo.createRecord") else {
+            throw NSError(domain: "", code: -1, userInfo: [NSLocalizedDescriptionKey: "Invalid URL"])
+        }
+
+        let likeRecord = FeedLike(
+            recordURI: strongReference.recordURI,
+            cidHash: strongReference.cidHash,
+            createdAt: createdAt
+        )
+
+        let requestBody = LikeRecordRequestBody(
+            repo: session.atDID,
+            record: likeRecord
+        )
+
+        
+    }
+
+    struct LikeRecordRequestBody: Encodable {
+        let repo: String
+        let collection: String = "app.bsky.feed.like"
+        let record: FeedLike
+    }
+}
