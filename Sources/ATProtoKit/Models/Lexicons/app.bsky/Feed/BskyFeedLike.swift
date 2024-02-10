a91ba8b84707fc59a54f3117d2e7dfefb8a7e384
Christopher Jr Riley <chrisjr@cjrriley.com>
2024-02-09 23:02:03 -0500

    Add model and implementation for like records

---
+++ Sources/ATProtoKit/Models/Lexicons/app.bsky/Feed/BskyFeedLike.swift
@@ -0,0 +1,40 @@
+//
+//  BskyFeedLike.swift
+//
+//
+//  Created by Christopher Jr Riley on 2024-02-08.
+//
+
+import Foundation
+
+public struct LikeRecord: Codable {
+    public let type: String = "app.bsky.feed.like"
+    public let subject: StrongReference
+    @DateFormatting public var createdAt: Date
+
+    public init(subject: StrongReference, createdAt: Date) {
+        self.subject = subject
+        self._createdAt = DateFormatting(wrappedValue: createdAt)
+    }
+
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+
+        self.subject = try container.decode(StrongReference.self, forKey: .subject)
+        self.createdAt = try container.decode(DateFormatting.self, forKey: .createdAt).wrappedValue
+    }
+
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+
+        try container.encode(self.type, forKey: .type)
+        try container.encode(self.subject, forKey: .subject)
+        try container.encode(self._createdAt, forKey: .createdAt)
+    }
+
+    enum CodingKeys: String, CodingKey {
+        case type = "$type"
+        case subject
+        case createdAt
+    }
+}
