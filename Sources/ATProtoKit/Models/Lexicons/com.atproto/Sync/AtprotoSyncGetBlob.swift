d145c4cf4a21f78f2751fb256a3dbf30e6102024
Christopher Jr Riley <chrisjr@cjrriley.com>
2024-02-10 14:00:15 -0500

    Add model and method for getting blobs

---
+++ Sources/ATProtoKit/Models/Lexicons/com.atproto/Sync/AtprotoSyncGetBlob.swift
@@ -0,0 +1,13 @@
+//
+//  File.swift
+//  
+//
+//  Created by Christopher Jr Riley on 2024-02-10.
+//
+
+import Foundation
+
+public struct BlobQuery: Encodable {
+    public let atDID: String
+    public let cidHash: String
+}
---
+++ Sources/ATProtoKit/Networking/Generic/com.atproto/GetBlob.swift
@@ -0,0 +1,26 @@
+//
+//  GetBlob.swift
+//
+//
+//  Created by Christopher Jr Riley on 2024-02-10.
+//
+
+import Foundation
+
+extension ATProtoKit {
+    public func getBlob(from blobQuery: BlobQuery) async -> Result<UploadBlobOutput, Error> {
+        guard let sessionURL = session.pdsURL,
+              let requestURL = URL(string: "\(sessionURL)/xrpc/com.atproto.sync.getBlob") else {
+            return .failure(NSError(domain: "", code: -1, userInfo: [NSLocalizedDescriptionKey: "Invalid URL"]))
+        }
+
+        let request = await APIClientService.createRequest(forRequest: requestURL, andMethod: .post, acceptValue: "'*/*'")
+
+        do {
+            let response = try await APIClientService.sendRequest(request, withEncodingBody: blobQuery, decodeTo: UploadBlobOutput.self)
+            return .success(response)
+        } catch {
+            return .failure(error)
+        }
+    }
+}
