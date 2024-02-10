56fddf7c06ea1830fe7f083e16bd23f40d7e3199
Christopher Jr Riley <chrisjr@cjrriley.com>
2024-02-10 11:02:45 -0500

    Add generic method for creating records

---
+++ Sources/ATProtoKit/Networking/API/com.atproto/CreateRecord.swift
@@ -0,0 +1,26 @@
+//
+//  CreateRecord.swift
+//
+//
+//  Created by Christopher Jr Riley on 2024-02-10.
+//
+
+import Foundation
+
+extension ATProtoKit {
+    public func createRecord<T: Encodable>(collection: String, requestBody: T, createdAt: Date = Date.now) async -> Result<StrongReference, Error> {
+        guard let sessionURL = session.pdsURL,
+              let requestURL = URL(string: "\(sessionURL)/xrpc/com.atproto.repo.createRecord") else {
+            return .failure(NSError(domain: "", code: -1, userInfo: [NSLocalizedDescriptionKey: "Invalid URL"]))
+        }
+        
+        let request = APIClientService.createRequest(forRequest: requestURL, andMethod: .post, authorizationValue: "Bearer \(session.accessToken)")
+        
+        do {
+            let result = try await APIClientService.sendRequest(request, withEncodingBody: requestBody, decodeTo: StrongReference.self)
+            return .success(result)
+        } catch {
+            return .failure(error)
+        }
+    }
+}
