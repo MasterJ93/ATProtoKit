aaa75671f0960c0d1957569ea494e5eddf43c958
Christopher Jr Riley <chrisjr@cjrriley.com>
2024-02-12 13:43:14 -0500

    Add documentation for ATProtoKit main method

--- Sources/ATProtoKit/ATProtoKit.swift
+++ Sources/ATProtoKit/ATProtoKit.swift
@@ -1,7 +1,35 @@
 import Foundation
 
+/// The base class that handles the main functionality of the `ATProtoKit` API library.
+///
+/// For methods which require authentication using an access token, instantiating `ATProtoKit` is required. To get the access token,
+/// an instance of ``ATProtocolConfiguration`` is required:
+///
+/// ```swift
+/// let config = ATProtocolConfiguration(handle: "example.bsky.social", appPassword: "hunter2")
+/// ```
+/// We then use ``ATProtocolConfiguration/authenticate()`` to grab the session information. The result is handed over to the `ATProtoKit`'s instance:
+///
+/// ```swift
+/// Task {
+///     print("Starting application...")
+///     let result = try await config.authenticate()
+///
+///     switch result {
+///         case .success(let result):
+///             print("Access token: \(result.accessToken)")
+///         case .failure(let error):
+///             print("Error: \(error)")
+///     }
+/// }
+/// ```
 public class ATProtoKit {
+    /// Represents an authenticated user session within the AT Protocol.
     let session: UserSession
+
+    /// Initializes a new instance of `ATProtoKit`.
+    /// - Parameters:
+    ///   - session: The authenticated user session within the AT Protocol.
     public init(session: UserSession) {
         self.session = session
     }
