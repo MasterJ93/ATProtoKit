4310f180cd6de2a06ee132fc9e44fadefdf92ad6
Christopher Jr Riley <chrisjr@cjrriley.com>
2024-02-13 21:55:26 -0500

    Add documentation for BskyActorDefs
    Also slightly modified some words in order to
    better fit the Swift API Design Guidelines.

--- Sources/ATProtoKit/Models/Lexicons/app.bsky/Actor/BskyActorDefs.swift
+++ Sources/ATProtoKit/Models/Lexicons/app.bsky/Actor/BskyActorDefs.swift
@@ -6,12 +6,27 @@
 //
 import Foundation
 
+/// A data model for a basic profile view definition.
+///
+/// - Note: This is based on the `app.bsky.actor.defs` lexicon.
+///
+/// The lexicon can be viewed in their [GitHub repo][github].
+///
+/// [github]: https://github.com/bluesky-social/atproto/blob/9579bec720d30e40c995d09772040212c261d6fb/lexicons/app/bsky/actor/defs.json
 public struct ProfileViewBasic: Codable {
+    /// The decentralized identifier (DID) of the user.
     public let atDID: String
+    /// The unique handle of the user.
     public let actorHandle: String
+    /// The display name of the user. Optional. Current maximum length is 64 characters.
     public var displayName: String? = nil
+    /// The avatar image of the user's profile. Optional.
     public var avatar: String? = nil
+    /// The viewer state of the user. Optional.
     public var viewer: ActorViewerState? = nil
+    /// An array of self-defined [...]. Optional.
+    ///
+    /// - ToDo: Figure out what this is about.
     public var labels: [Label]? = nil
 
     public init(atDID: String, actorHandle: String, displayName: String?, avatar: String?, viewer: ActorViewerState?, labels: [Label]?) {
@@ -58,14 +73,29 @@ public struct ProfileViewBasic: Codable {
     }
 }
 
+/// A data model for a profile view definition.
+///
+/// - Note: This is based on the `app.bsky.actor.defs` lexicon.
+///
+/// The lexicon can be viewed in the AT Protocol's [GitHub repo][github].
+///
+/// [github]: https://github.com/bluesky-social/atproto/blob/9579bec720d30e40c995d09772040212c261d6fb/lexicons/app/bsky/actor/defs.json
 public struct ProfileView: Codable {
+    /// The decentralized identifier (DID) of the user.
     public let atDID: String
+    /// The unique handle of the user.
     public let actorHandle: String
+    /// The display name of the user's profile. Optional. Current maximum length is 64 characters.
     public var displayName: String? = nil
+    /// The description of the user's profile. Optional. Current maximum length is 256 characters.
     public var description: String? = nil
+    /// The avatar image of a user's profile. Optional.
     public let avatar: String?
+    /// The date the profile was last indexed. Optional.
     @DateFormattingOptional public var indexedAt: Date? = nil
+    /// <#Description#>
     public var viewer: ActorViewerState? = nil
+    /// <#Description#>
     public var labels: [Label]? = nil
 
     public init(atDID: String, actorHandle: String, displayName: String?, description: String?, avatar: String?, indexedAt: Date?, viewer: ActorViewerState?, labels: [Label]?) {
@@ -123,18 +153,39 @@ public struct ProfileView: Codable {
     }
 }
 
+/// A data model for a detailed profile view definition.
+///
+/// - Note: This is based on the `app.bsky.actor.defs` lexicon.
+///
+/// The lexicon can be viewed in the AT Protocol's [GitHub repo][github].
+///
+/// [github]: https://github.com/bluesky-social/atproto/blob/9579bec720d30e40c995d09772040212c261d6fb/lexicons/app/bsky/actor/defs.json
 public struct ProfileViewDetailed: Codable {
+    /// The decentralized identifier (DID) of the user.
     public let atDID: String
+    /// The unique handle of the user.
     public let actorHandle: String
+    /// The display name of the user's profile. Optional. Current maximum length is 64 characters.
     public var displayName: String? = nil
+    /// The description of the user's profile. Optional. Current maximum length is 256 characters.
     public var description: String? = nil
+    /// The avatar image of a user's profile. Optional.
     public var avatar: String? = nil
+    /// The banner image of a user's profile. Optional.
     public var banner: String? = nil
+    /// The number of followers a user has. Optional.
     public var followerCount: Int? = nil
+    /// The number of accounts the user follows. Optional.
     public var followCount: Int? = nil
+    /// The number of posts the user has. Optional.
     public var postCount: Int? = nil
+    /// The date the profile was last indexed. Optional.
     @DateFormattingOptional public var indexedAt: Date? = nil
+    /// The viewer state of the user. Optional.
     public var viewer: ActorViewerState? = nil
+    /// An array of self-defined [...]. Optional.
+    ///
+    /// - ToDo: Figure out what this is about.
     public var labels: [Label]? = nil
 
     public init(atDID: String, actorHandle: String, displayName: String?, description: String?, avatar: String?, banner: String?, followerCount: Int?, followCount: Int?, postCount: Int?, indexedAt: Date?, viewer: ActorViewerState?, labels: [Label]?) {
@@ -208,32 +259,68 @@ public struct ProfileViewDetailed: Codable {
     }
 }
 
+/// A data model for an actor viewer state definition.
+///
+/// - Note: From the AT Protocol specification: "Metadata about the requesting account's relationship with the subject account. Only has meaningful content for authed requests."
+/// - Note: This is based on the `app.bsky.actor.defs` lexicon.
+///
+/// The lexicon can be viewed in the AT Protocol's [GitHub repo][github].
+///
+/// [github]: https://github.com/bluesky-social/atproto/blob/9579bec720d30e40c995d09772040212c261d6fb/lexicons/app/bsky/actor/defs.json
 public struct ActorViewerState: Codable {
+    /// Indicates whether the requesting account has been muted by the subject account. Optional.
     public let isMuted: Bool? = nil
-    public let mutedByList: ListViewBasic? = nil
-    public let blockedBy: Bool? = nil
+    /// An array of
+    public let mutedByArray: ListViewBasic? = nil
+    /// Indicates whether the requesting account has been blocked by the subject account. Optional.
+    public let isBlocked: Bool? = nil
+    /// A URI.
+    ///
+    /// - ToDo: Figure out what this is about.
     public let blocking: String? = nil
-    public let blockingByList: ListViewBasic? = nil
+    /// An array of the subject account's lists.
+    public let blockingByArray: ListViewBasic? = nil
+    /// A URI.
+    ///
+    /// - ToDo: Figure out what this is about.
     public let following: String? = nil
+    /// A URI.
+    ///
+    /// - ToDo: Figure out what this is about.
     public let followedBy: String? = nil
 
     enum CodingKeys: String, CodingKey {
         case isMuted = "muted"
-        case mutedByList = "mutedByList"
-        case blockedBy = "blockedBy"
-        case blocking = "blocking"
-        case blockingByList = "blockingByList"
+        case mutedByArray = "mutedByList"
+        case isBlocked = "blockedBy"
+        case blocking
+        case blockingByArray = "blockingByList"
         case following = "following"
         case followedBy = "followedBy"
     }
 }
 
-// Define structs for each preference type
+/// A data model for a preferences definition.
+///
+///- Note: This is based on the `app.bsky.actor.defs` lexicon.
+///
+/// The lexicon can be viewed in the AT Protocol's [GitHub repo][github].
+///
+/// [github]: https://github.com/bluesky-social/atproto/blob/9579bec720d30e40c995d09772040212c261d6fb/lexicons/app/bsky/actor/defs.json
 public struct ActorPreferences: Codable {
+    /// An array of different preferences the user can view and set.
     let preferences: [ActorPreferenceUnion]
 }
 
+/// A data model for an "Adult Content" preference definition.
+///
+/// - Note: This is based on the `app.bsky.actor.defs` lexicon.
+///
+/// The lexicon can be viewed in the AT Protocol's [GitHub repo][github].
+///
+/// [github]: https://github.com/bluesky-social/atproto/blob/9579bec720d30e40c995d09772040212c261d6fb/lexicons/app/bsky/actor/defs.json
 public struct AdultContentPreferences: Codable {
+    /// Indicates whether the user will be able to see adult content in their feed. Set to `false` by default.
     var isAdultContentEnabled: Bool = false
 
     enum CodingKeys: String, CodingKey {
@@ -241,23 +328,55 @@ public struct AdultContentPreferences: Codable {
     }
 }
 
+/// A data model for a "Content Label" preference defintion.
+///
+/// - Note: This is based on the `app.bsky.actor.defs` lexicon.
+///
+/// The lexicon can be viewed in the AT Protocol's [GitHub repo][github].
+///
+/// [github]: https://github.com/bluesky-social/atproto/blob/9579bec720d30e40c995d09772040212c261d6fb/lexicons/app/bsky/actor/defs.json
 public struct ContentLabelPreferences: Codable {
+    /// The name of the content label.
     let label: String
+    /// Indicates the visibility of the label's content.
     let visibility: Visibility
 
+    /// Determines how visible a label's content is.
     enum Visibility: String, Codable {
+        /// Indicates the content can be seen without restriction.
         case show = "show"
+        /// Indicates the content can be seen, but will ask if the user wants to view it.
         case warn = "warn"
+        /// Indicates the content is fully invisible by the user.
         case hide = "hide"
     }
 }
 
+/// A data model for a "Saved Feeds" preference definition.
+///
+/// - Note: This is based on the `app.bsky.actor.defs` lexicon.
+///
+/// The lexicon can be viewed in the AT Protocol's [GitHub repo][github].
+///
+/// [github]: https://github.com/bluesky-social/atproto/blob/9579bec720d30e40c995d09772040212c261d6fb/lexicons/app/bsky/actor/defs.json
 public struct SavedFeedsPreferences: Codable {
+    /// An array of feeds that have been saved and pinned.
     let pinned: [String]
+    /// An array of feeds that have been saved.
     let saved: [String]
 }
 
+/// A data model for a "Personal Details" preference definition.
+///
+/// - Note: This is based on the `app.bsky.actor.defs` lexicon.
+///
+/// The lexicon can be viewed in the AT Protocol's [GitHub repo][github].
+///
+/// [github]: https://github.com/bluesky-social/atproto/blob/9579bec720d30e40c995d09772040212c261d6fb/lexicons/app/bsky/actor/defs.json
 public struct PersonalDetailsPreferences: Codable {
+    /// The birth date of the user. Optional.
+    ///
+    /// - Note: From the AT Protocol specification: "The birth date of account owner."
     @DateFormattingOptional var birthDate: Date? = nil
 
     public init(birthDate: Date) {
@@ -281,12 +400,37 @@ public struct PersonalDetailsPreferences: Codable {
     }
 }
 
+/// A data model for a "Feed View" preference definition.
+///
+/// - Note: This is based on the `app.bsky.actor.defs` lexicon.
+///
+/// The lexicon can be viewed in the AT Protocol's [GitHub repo][github].
+///
+/// [github]: https://github.com/bluesky-social/atproto/blob/9579bec720d30e40c995d09772040212c261d6fb/lexicons/app/bsky/actor/defs.json
 public struct FeedViewPreferences: Codable {
+    /// The feed's identifier (typically the URI).
+    ///
+    /// - Note:From the AT Protocol specification: "The URI of the feed, or an identifier which describes the feed."
     let feed: String
+    /// Indicates whether the replies are hidden from the user. Optional.
+    ///
+    /// - Note: From the AT Protocol specification: "Hide replies in the feed."
     let areRepliesHidden: Bool? = nil
+    /// Indicates whether replies from users you don't follow are hidden from the user. Optional.
+    ///
+    /// - Note: From the AT Protocol specification: "Hide replies in the feed if they are not by followed users."
     let areUnfollowedRepliesHidden: Bool? = nil
+    /// Indicates how many likes a post needs in order for the user to see the reply. Optional.
+    ///
+    /// - Note: From the AT Protocol specification: "Hide replies in the feed if they do not have this number of likes."
     let hideRepliesByLikeCount: Int? = nil
+    /// Indicates whether reposts are hidden from the user. Optional.
+    ///
+    /// - Note: From the AT Protocol specification: "Hide reposts in the feed."
     let areRepostsHidden: Bool? = nil
+    /// Indicates whether quote posts are hidden from the user. Optional.
+    ///
+    /// - Note: From the AT Protocol specification: "Hide quote posts in the feed."
     let areQuotePostsHidden: Bool? = nil
 
     enum CodingKeys: String, CodingKey {
@@ -299,26 +443,51 @@ public struct FeedViewPreferences: Codable {
     }
 }
 
+/// A data model for a "Thread View" preference definition.
+///
+/// - Note: This is based on the `app.bsky.actor.defs` lexicon.
+///
+/// The lexicon can be viewed in the AT Protocol's [GitHub repo][github].
+///
+/// [github]: https://github.com/bluesky-social/atproto/blob/9579bec720d30e40c995d09772040212c261d6fb/lexicons/app/bsky/actor/defs.json
 public struct ThreadViewPreferences: Codable {
-    let sort: SortingMode? = nil
+    /// The sorting mode of a thread. Optional.
+    ///
+    /// - Note: From the AT Protocol specification: "Sorting mode for threads."
+    let sortingMode: SortingMode? = nil
+    /// Indicates whether users you follow are prioritized over other users. Optional.
+    ///
+    /// - Note: From the AT Protocol specification: "Show followed users at the top of all replies."
     let areFollowedUsersPrioritized: Bool? = nil
 
+    /// The sorting mode for a thread.
     enum SortingMode: String, Codable {
+        /// Indicates the thread will be sorted from the oldest post.
         case oldest = "oldest"
+        /// Indicates the thread will be sorted from the newest post.
         case newest = "newest"
+        /// Indicates the thread will be sorted from the posts with the most number of likes.
         case mostLikes = "most-likes"
+        /// Indicates the thread will be completely random.
         case random = "random"
     }
 
     enum CodingKeys: String, CodingKey {
-        case sort
+        case sortingMode = "sort"
         case areFollowedUsersPrioritized = "prioritizeFollowedUsers"
     }
 }
 
-/// A preference object for the user's preferences for interests.
+/// A data model for an "Interest View" preference definition.
+///
+/// - Note: This is based on the `app.bsky.actor.defs` lexicon.
+///
+/// The lexicon can be viewed in the AT Protocol's [GitHub repo][github].
+///
+/// [github]: https://github.com/bluesky-social/atproto/blob/9579bec720d30e40c995d09772040212c261d6fb/lexicons/app/bsky/actor/defs.json
 public struct InterestViewPreferences: Codable {
     /// An array of interest tags. Current maximum limit is 100 tags.
+    ///
     /// - Note: According to AT Protocol's specifications: "A list of tags which describe the account owner's interests gathered during onboarding."
     public let tags: [String]
 
@@ -335,9 +504,9 @@ public struct InterestViewPreferences: Codable {
     public func encode(to encoder: Encoder) throws {
         var container = encoder.container(keyedBy: CodingKeys.self)
 
-        // Truncate `tags` to 640 characters before encoding
-        // `maxGraphemes`'s limit is 64, but `String.count` should respect that limit implictly
-        // Then, truncate `tags` to 100 items before encoding
+        // Truncate `tags` to 640 characters before encoding.
+        // `maxGraphemes`'s limit is 64, but `String.count` should respect that limit implictly.
+        // Then, truncate `tags` to 100 items before encoding.
         let truncatedTags = self.tags.map { $0.truncated(toLength: 640) }
         try truncatedEncode(truncatedTags, withContainer: &container, forKey: .tags, upToLength: 100)
     }
@@ -348,13 +517,22 @@ public struct InterestViewPreferences: Codable {
 }
 
 // Define an enum to represent the possible preference types
+/// A reference containing the list of preferences.
 public enum ActorPreferenceUnion: Codable {
+    /// The "Adult Content" preference.
     case adultContent(AdultContentPreferences)
+    /// The "Content Label" preference.
     case contentLabel(ContentLabelPreferences)
+    /// The "Saved Feeds" preference.
     case savedFeeds(SavedFeedsPreferences)
+    /// The "Personal Details" preference.
     case personalDetails(PersonalDetailsPreferences)
+    /// The "Feed View" preference.
     case feedView(FeedViewPreferences)
+    /// The "Thread View" preference.
     case threadView(ThreadViewPreferences)
+    /// The "Interest View" preference.
+    case interestViewPreferences(InterestViewPreferences)
 
     // Implement custom decoding
     public init(from decoder: Decoder) throws {
