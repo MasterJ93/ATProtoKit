9f9769abed63a527b344bca347a96e9e67290988
Christopher Jr Riley <chrisjr@cjrriley.com>
2024-02-14 13:54:03 -0500

    Add documentation for BskyRecordWithMedia

--- Sources/ATProtoKit/Models/Lexicons/app.bsky/Embed/BskyEmbedRecordWithMedia.swift
+++ Sources/ATProtoKit/Models/Lexicons/app.bsky/Embed/BskyEmbedRecordWithMedia.swift
@@ -7,9 +7,21 @@
 
 import Foundation
 
+// MARK: - Main definition
+/// The main data model definition for a record embedded with some form of compatible media.
+///
+/// - Note: According to the AT Protocol specifications: "A representation of a record embedded in a Bluesky record (eg, a post), alongside other compatible embeds. For example, a quote post and image, or a quote post and external URL card."
+/// - SeeAlso: This is based on the [`app.bsky.embed.recordWithMedia][github] lexicon.
+///
+/// [github]: https://github.com/bluesky-social/atproto/blob/main/lexicons/app/bsky/embed/recordWithMedia.json
 public struct EmbedRecordWithMedia: Codable {
+    /// The identifier of the lexicon.
+    ///
+    /// - Warning: The value must not change.
     public let type: String = "app.bsky.embed.recordWithMedia"
+    /// The record that will be embedded.
     public let record: EmbedRecord
+    /// The media of a specific type.
     public let media: MediaUnion
 
     enum CodingKeys: String, CodingKey {
@@ -19,13 +31,29 @@ public struct EmbedRecordWithMedia: Codable {
     }
 }
 
+// MARK: -
+/// A data model for a definition which contains an embedded record and embedded media.
+///
+/// - SeeAlso: This is based on the [`app.bsky.embed.recordWithMedia`][github] lexicon.
+///
+/// [github]: https://github.com/bluesky-social/atproto/blob/main/lexicons/app/bsky/embed/recordWithMedia.json
 public struct EmbedRecordWithMediaView: Codable {
+    /// The embeded record.
     public let record: EmbedRecordView
+    /// The embedded media.
     public let media: MediaViewUnion
 }
 
+// MARK: - Union Types
+/// A reference containing the list of the types of compatible media.
+///
+/// - SeeAlso: This is based on the [`app.bsky.embed.recordWithMedia`][github] lexicon.
+///
+/// [github]: https://github.com/bluesky-social/atproto/blob/main/lexicons/app/bsky/embed/recordWithMedia.json
 public enum MediaUnion: Codable {
+    /// An image that will be embedded.
     case embedImages(EmbedImages)
+    /// An external link that will be embedded.
     case embedExternal(EmbedExternal)
 
     public init(from decoder: Decoder) throws {
@@ -52,8 +80,15 @@ public enum MediaUnion: Codable {
     }
 }
 
+/// A reference containing the list of the types of compatible media that can be viewed.
+///
+/// - SeeAlso: This is based on the [`app.bsky.embed.recordWithMedia`][github] lexicon.
+///
+/// [github]: https://github.com/bluesky-social/atproto/blob/main/lexicons/app/bsky/embed/recordWithMedia.json
 public enum MediaViewUnion: Codable {
+    /// An image that's been embedded.
     case embedImagesView(EmbedImagesView)
+    /// An external link that's been embedded.
     case embedExternalView(EmbedExternalView)
 
     public init(from decoder: Decoder) throws {
