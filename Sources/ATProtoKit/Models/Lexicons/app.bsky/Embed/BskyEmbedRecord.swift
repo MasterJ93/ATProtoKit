60d5ecc0bd43b63a4bdc9847f39399e75c8e21c7
Christopher Jr Riley <chrisjr@cjrriley.com>
2024-02-14 13:08:48 -0500

    Add documentation for BskyEmbedRecord
    - Tweaked some variables to fit the Swift API Design
    Guidelines.
    - Replaced RecordViewUnion in the
    `ViewRecord.embeds` variable with EmbedViewUnion.

--- Sources/ATProtoKit/Models/Lexicons/app.bsky/Embed/BskyEmbedRecord.swift
+++ Sources/ATProtoKit/Models/Lexicons/app.bsky/Embed/BskyEmbedRecord.swift
@@ -7,8 +7,21 @@
 
 import Foundation
 
+// MARK: - Main definition
+/// The main data model definition for record embeds.
+///
+/// - Note: According to the AT Protocol specifications: "A representation of a record embedded in a Bluesky record (eg, a post). For example, a quote-post, or sharing a feed generator record."
+/// - Note: This is based on the `app.bsky.embed.record` lexicon.
+///
+/// The lexicon can be viewed in their [GitHub repo][github].
+///
+/// [github]: https://github.com/bluesky-social/atproto/blob/main/lexicons/app/bsky/embed/record.json
 public struct EmbedRecord: Codable {
+    /// The identifier of the lexicon.
+    ///
+    /// - Warning: The value must not change.
     public let type: String = "app.bsky.embed.record"
+    /// The strong reference of the record.
     public let record: StrongReference
 
     enum CodingKeys: String, CodingKey {
@@ -17,7 +30,16 @@ public struct EmbedRecord: Codable {
     }
 }
 
+// MARK: -
+/// A data model for a view definition.
+///
+/// - Note: This is based on the `app.bsky.embed.record` lexicon.
+///
+/// The lexicon can be viewed in their [GitHub repo][github].
+///
+/// [github]: https://github.com/bluesky-social/atproto/blob/main/lexicons/app/bsky/embed/record.json
 public struct EmbedRecordView: Codable {
+    /// The record of a specific type.
     public let record: RecordViewUnion
 
     public init(record: RecordViewUnion) {
@@ -25,13 +47,30 @@ public struct EmbedRecordView: Codable {
     }
 }
 
+/// A data model for a record definition in an embed.
+///
+/// - Note: This is based on the `app.bsky.embed.record` lexicon.
+///
+/// The lexicon can be viewed in their [GitHub repo][github].
+///
+/// [github]: https://github.com/bluesky-social/atproto/blob/main/lexicons/app/bsky/embed/record.json
 public struct ViewRecord: Codable {
+    /// The URI of the record.
     public let recordURI: String
+    /// The CID of the record.
     public let cidHash: String
+    /// The creator of the record.
     public let author: ProfileViewBasic
+    // TODO: Find out what specific type falls under this variable.
+    /// The value of the record.
+    ///
+    /// - Note: According to the AT Protocol specifications: "The record data itself."
     public let value: UnknownType
+    /// An array of labels attached to the record.
     public let labels: [Label]?
-    public let embeds: [RecordViewUnion]?
+    /// An array of embed views of various types..
+    public let embeds: [EmbedViewUnion]?
+    /// The date the record was last indexed.
     @DateFormatting public var indexedAt: Date
 
     enum CodingKeys: String, CodingKey {
@@ -45,33 +84,65 @@ public struct ViewRecord: Codable {
     }
 }
 
+/// A data model for a definition of a record that was unable to be found.
+///
+/// - Note: This is based on the `app.bsky.embed.record` lexicon.
+///
+/// The lexicon can be viewed in their [GitHub repo][github].
+///
+/// [github]: https://github.com/bluesky-social/atproto/blob/main/lexicons/app/bsky/embed/record.json
 public struct ViewNotFound: Codable {
+    /// The URI of the record.
     public let recordURI: String
-    public let isNotFound: Bool
+    /// Indicates whether the record was found.
+    public let isRecordNotFound: Bool
 
     enum CodingKeys: String, CodingKey {
         case recordURI = "uri"
-        case isNotFound = "notFound"
+        case isRecordNotFound = "notFound"
     }
 }
 
+/// A data model for a definition of a record that has been blocked.
+///
+/// - Note: This is based on the `app.bsky.embed.record` lexicon.
+///
+/// The lexicon can be viewed in their [GitHub repo][github].
+///
+/// [github]: https://github.com/bluesky-social/atproto/blob/main/lexicons/app/bsky/embed/record.json
 public struct ViewBlocked: Codable {
+    /// The URI of the record.
     public let recordURI: String
-    public let isBlocked: Bool
-    public let author: BlockedAuthor
+    /// Indicates whether the record has been blocked.
+    public let isRecordBlocked: Bool
+    /// The author of the record.
+    public let recordAuthor: BlockedAuthor
 
     enum CodingKeys: String, CodingKey {
         case recordURI = "uri"
-        case isBlocked = "blocked"
-        case author = "author"
+        case isRecordBlocked = "blocked"
+        case recordAuthor = "author"
     }
 }
 
+// MARK: - Union types
+/// A reference containing the list of the status of a record.
+///
+/// - Note: This is based on the `app.bsky.embed.record` lexicon.
+///
+/// The lexicon can be viewed in their [GitHub repo][github].
+///
+/// [github]: https://github.com/bluesky-social/atproto/blob/main/lexicons/app/bsky/embed/record.json
 public enum RecordViewUnion: Codable {
+    /// A normal record type.
     case viewRecord(ViewRecord)
+    /// A record that may not have been found.
     case viewNotFound(ViewNotFound)
+    /// A record that may have been blocked.
     case viewBlocked(ViewBlocked)
+    /// A record that contains a generator view.
     case generatorView(GeneratorView)
+    /// A record that contains a list view.
     case listView(ListView)
 
     public init(from decoder: Decoder) throws {
