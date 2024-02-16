a45de2a585db9f3925d6a01f66e275834dee06e1
Christopher Jr Riley <chrisjr@cjrriley.com>
2024-02-16 13:31:58 -0500

    Add documentation for createRecord

--- Sources/ATProtoKit/Networking/CoreAPI/com.atproto/CreateRecord.swift
+++ Sources/ATProtoKit/Networking/CoreAPI/com.atproto/CreateRecord.swift
@@ -8,12 +8,12 @@
 import Foundation
 
 extension ATProtoKit {
-    /// <#Description#>
+    /// Creates a record attached to a user's account.
     /// - Parameters:
-    ///   - collection: <#collection description#>
-    ///   - requestBody: <#requestBody description#>
-    ///   - createdAt: <#createdAt description#>
-    /// - Returns: <#description#>
+    ///   - collection: The NSID of the record.
+    ///   - requestBody: The request body that contains the information needed to create the specific type of record.
+    ///   - createdAt: The date and time the record was created. Defaults to the date and time this method was called.
+    /// - Returns: A `Result`, containing either a ``StrongReference`` if successful, and an `Error` if not.
     public func createRecord<T: Encodable>(collection: String, requestBody: T, createdAt: Date = Date.now) async -> Result<StrongReference, Error> {
         guard let sessionURL = session.pdsURL,
               let requestURL = URL(string: "\(sessionURL)/xrpc/com.atproto.repo.createRecord") else {
