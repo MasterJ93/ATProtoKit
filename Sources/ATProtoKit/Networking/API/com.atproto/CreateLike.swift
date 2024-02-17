d2016b943daa9eb5f39c2242223fd5d98d6d524b
Christopher Jr Riley <chrisjr@cjrriley.com>
2024-02-16 23:02:40 -0500

    Add documentation
    Created some documentation to createLikeRecord and
    LikeRecordRequestBody.

--- Sources/ATProtoKit/Networking/API/com.atproto/CreateLike.swift
+++ Sources/ATProtoKit/Networking/API/com.atproto/CreateLike.swift
@@ -8,6 +8,11 @@
 import Foundation
 
 extension ATProtoKit {
+    /// Create a like record to a user's post.
+    /// - Parameters:
+    ///   - strongReference: The URI of the record, which contains the `recordURI` and `cidHash`.
+    ///   - createdAt: The date and time the like record was created. Defaults to `Date.now`.
+    /// - Returns: A `Result`, containing either a ``StrongReference`` if it's successful, or an `Error` if it's not.
     public func createLikeRecord(_ strongReference: StrongReference, createdAt: Date = Date.now) async throws -> Result<StrongReference, Error> {
         guard let sessionURL = session.pdsURL,
               let requestURL = URL(string: "\(sessionURL)/xrpc/com.atproto.repo.createRecord") else {
@@ -26,10 +31,16 @@ extension ATProtoKit {
 
         return await createRecord(collection: "app.bsky.feed.like", requestBody: requestBody)
     }
-
+    
+    /// The request body for a like record.
     struct LikeRecordRequestBody: Encodable {
+        /// The repository for the like record.
         let repo: String
+        /// The Namespaced Identifiers (NSID) of the request body.
+        ///
+        /// - Warning: The value must not change.
         let collection: String = "app.bsky.feed.like"
+        /// The like record itself.
         let record: FeedLike
     }
 }
