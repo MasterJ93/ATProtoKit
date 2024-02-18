9bc692ddab0d9329219a6944c787055731b36668
Christopher Jr Riley <chrisjr@cjrriley.com>
2024-02-18 08:21:49 -0500

    Add implementation for getSyncRecord
    It appears that this method is similar to getRepoRecord.
    However, it hasn't been tested yet. The API playground
    Bluesky has doesn't seem to work, but it's been speculated
    that it's not redirecting to bsky.social. This will therfore need
    to be tested on another server connected to AT Protocol.
    
    For now, this will be dormant until it can be tested and a
    full understanding of .cat files (including how to parse
    them) are made.

---
+++ Sources/ATProtoKit/Networking/CoreAPI/GetSyncRecord.swift
@@ -0,0 +1,42 @@
+//
+//  File.swift
+//  
+//
+//  Created by Christopher Jr Riley on 2024-02-18.
+//
+
+import Foundation
+
+extension ATProtoKit {
+    // TODO: Search up parsing .CAR files for implementation in this method.
+    public static func getSyncRecord(_ recordQuery: RecordQuery, pdsURL: String? = "https://bsky.social") async throws -> Result<Data, Error> {
+        guard let sessionURL = pdsURL,
+              let requestURL = URL(string: "\(sessionURL)/xrpc/com.atproto.sync.getRecord") else {
+            return .failure(URIError.invalidFormat)
+        }
+
+        do {
+            var queryItems = [
+                ("repo", recordQuery.repo),
+                ("collection", recordQuery.collection),
+                ("rkey", recordQuery.recordKey)
+            ]
+
+            if let cid = recordQuery.recordCID {
+                queryItems.append(("cid", cid))
+            }
+
+            let queryURL = try APIClientService.setQueryItems(
+                for: requestURL,
+                with: queryItems
+            )
+
+            let request = APIClientService.createRequest(forRequest: queryURL, andMethod: .get, authorizationValue: nil)
+
+            let response = try await APIClientService.sendRequest(request)
+            return .success(response)
+        } catch {
+            return .failure(error)
+        }
+    }
+}
