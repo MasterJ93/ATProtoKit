df6e8334acc4897180ef53999e9a4fa57870425c
Christopher Jr Riley <chrisjr@cjrriley.com>
2024-02-19 23:50:31 -0500

    Remove ActorGetPreferencesQuery
    This is pretty unneeded. As a result, this will be
    removed.

--- Sources/ATProtoKit/Models/Lexicons/app.bsky/Actor/BskyActorGetPreferences.swift
+++ Sources/ATProtoKit/Models/Lexicons/app.bsky/Actor/BskyActorGetPreferences.swift
@@ -7,20 +7,10 @@
 
 import Foundation
 
-// MARK: - Main definition
-/// The main data model definition for external embeds.
-///
-/// - Note: According to the AT Protocol specifications: "Get private preferences attached to the current account. Expected use is synchronization between multiple devices, and import/export during account migration. Requires auth."
-/// - SeeAlso: This is based on the [`app.bsky.actor.getPreferences`][github] lexicon.
-///
-/// [github]: https://github.com/bluesky-social/atproto/blob/main/lexicons/app/bsky/actor/getPreferences.json
-public struct ActorGetPreferencesQuery: Codable {
-
-}
-
 // MARK: -
 /// A data model definition for the output of getting preferences.
 ///
+/// - Note: According to the AT Protocol specifications: "Get private preferences attached to the current account. Expected use is synchronization between multiple devices, and import/export during account migration. Requires auth."
 /// - SeeAlso: This is based on the [`app.bsky.actor.getPreferences`][github] lexicon.
 ///
 /// [github]: https://github.com/bluesky-social/atproto/blob/main/lexicons/app/bsky/actor/getPreferences.json
