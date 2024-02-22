0bea9a8d55d3592a8bd728e4535d52a0521ee9c6
Christopher Jr Riley <chrisjr@cjrriley.com>
2024-02-22 01:14:15 -0500

    Add documentation for AtprotoRepoGetRecord

--- Sources/ATProtoKit/Models/Lexicons/com.atproto/Repo/AtprotoRepoGetRecord.swift
+++ Sources/ATProtoKit/Models/Lexicons/com.atproto/Repo/AtprotoRepoGetRecord.swift
@@ -7,10 +7,30 @@
 
 import Foundation
 
+// MARK: - Main definition
+/// The main data model definition for a record.
+///
+/// - Note: According to the AT Protocol specifications: "Get a single record from a repository. Does not require auth."
+///
+/// - SeeAlso: This is based on the [`com.atproto.repo.getRecord`][github] lexicon.
+///
+/// [github]: https://github.com/bluesky-social/atproto/blob/main/lexicons/com/atproto/repo/getRecord.json
 public struct RecordQuery: Codable {
+    /// The handle or decentralized identifier (DID) of the repo."
+    ///
+    /// - Note: According to the AT Protocol specifications: "The handle or DID of the repo."
     public let repo: String
+    /// The NSID of the record.
+    ///
+    /// - Note: According to the AT Protocol specifications: "The NSID of the record collection."
     public let collection: String
+    /// The record's key.
+    ///
+    //// - Note: According to the AT Protocol specifications: "The Record Key."
     public let recordKey: String
+    /// The CID of the version of the record. Optional. If not specified, then return the most recent version.
+    ///
+    /// - Note: According to the AT Protocol specifications: "The CID of the version of the record. If not specified, then return the most recent version."
     public let recordCID: String? = nil
 
     public init(repo: String, collection: String, recordKey: String) {
@@ -27,18 +47,34 @@ public struct RecordQuery: Codable {
     }
 }
 
+// MARK: -
+/// The main data model definition for the outpot of a record.
+///
+/// - SeeAlso: This is based on the [`com.atproto.repo.getRecord`][github] lexicon.
+///
+/// [github]: https://github.com/bluesky-social/atproto/blob/main/lexicons/com/atproto/repo/getRecord.json
 public struct RecordOutput: Codable {
-    public let atURI: String
+    /// The URI of the record.
+    public let recordURI: String
+    /// The CID hash for the record.
     public let recordCID: String
+    /// The value for the record.
     public let value: RecordValueReply?
 
     enum CodingKeys: String, CodingKey {
-        case atURI = "uri"
+        case recordURI = "uri"
         case recordCID = "cid"
         case value = "value"
     }
 }
 
+// MARK: -
+/// The main data model definition for the outpot .
+///
+/// - SeeAlso: This is based on the [`com.atproto.repo.getRecord`][github] lexicon.
+///
+/// [github]: https://github.com/bluesky-social/atproto/blob/main/lexicons/com/atproto/repo/getRecord.json
 public struct RecordValueReply: Codable {
+    /// The reply reference of the record.
     public let reply: ReplyReference?
 }
