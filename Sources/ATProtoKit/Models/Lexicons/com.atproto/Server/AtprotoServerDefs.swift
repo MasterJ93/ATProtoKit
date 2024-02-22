1fe8f1d406d78eb3bb640ed639574e556ad92d56
Christopher Jr Riley <chrisjr@cjrriley.com>
2024-02-22 02:06:03 -0500

    Add documentation for AtprotoServerDefs

--- Sources/ATProtoKit/Models/Lexicons/com.atproto/Server/AtprotoServerDefs.swift
+++ Sources/ATProtoKit/Models/Lexicons/com.atproto/Server/AtprotoServerDefs.swift
@@ -7,13 +7,26 @@
 
 import Foundation
 
+// MARK: - Main definition
+/// The main data model definition for a server invite code.
+///
+/// - SeeAlso: This is based on the [`com.atproto.server.defs`][github] lexicon.
+///
+/// [github]: https://github.com/bluesky-social/atproto/blob/main/lexicons/com/atproto/server/defs.json
 public struct ServerInviteCode: Codable {
+    /// The actual invite code.
     public let code: String
+    /// The number of codes available.
     public let available: Int
+    /// Indicates whether the invite code is disabled.
     public let isDisabled: Bool
+    /// The user who holds the invite codes.
     public let forAccount: String
+    /// The name of the user who currently holds the account.
     public let createdBy: String
+    /// The date and time the invite codes were created.
     @DateFormatting public var createdAt: Date
+    /// An array of the invite code uses.
     public let uses: [ServerInviteCodeUse]
 
     public init(code: String, available: Int, isDisabled: Bool, forAccount: String, createdBy: String, createdAt: Date, uses: [ServerInviteCodeUse]) {
@@ -61,8 +74,15 @@ public struct ServerInviteCode: Codable {
     }
 }
 
+/// A data model for a definition for the invite code's use.
+///
+/// - SeeAlso: This is based on the [`com.atproto.server.defs`][github] lexicon.
+///
+/// [github]: https://github.com/bluesky-social/atproto/blob/main/lexicons/com/atproto/server/defs.json
 public struct ServerInviteCodeUse: Codable {
+    /// Who used the invite code.
     public let usedBy: String
+    /// The date and time the service code was used.
     @DateFormatting public var usedAt: Date
 
     public init(usedBy: String, usedAt: Date) {
