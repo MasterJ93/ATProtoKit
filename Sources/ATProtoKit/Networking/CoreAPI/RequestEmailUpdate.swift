e8c0b6fc4906a179116804f4a54f777275bee4ea
Christopher Jr Riley <chrisjr@cjrriley.com>
2024-02-26 22:12:28 -0500

    Add RequestEmailUpdate

---
+++ Sources/ATProtoKit/Networking/CoreAPI/RequestEmailUpdate.swift
@@ -0,0 +1,29 @@
+//
+//  RequestEmailUpdate.swift
+//
+//
+//  Created by Christopher Jr Riley on 2024-02-26.
+//
+
+import Foundation
+
+extension ATProtoKit {
+    /// Requests a token to be emailed to the user in order to update their email address.
+    /// 
+    /// - Returns: A `Result`, containing either a ``RequestEmailUpdateOutput`` if successful, or an `Error` if not.
+    public func requestEmailUpdate() async throws -> Result<RequestEmailUpdateOutput, Error> {
+        guard let sessionURL = session.pdsURL,
+              let requestURL = URL(string: "\(sessionURL)/xrpc/om.atproto.server.requestEmailUpdate") else {
+            return .failure(NSError(domain: "", code: -1, userInfo: [NSLocalizedDescriptionKey : "Invalid URL"]))
+        }
+
+        do {
+            let request = APIClientService.createRequest(forRequest: requestURL, andMethod: .post, acceptValue: "application/json", contentTypeValue: nil, authorizationValue: "Bearer \(session.accessToken)")
+            let response = try await APIClientService.sendRequest(request, decodeTo: RequestEmailUpdateOutput.self)
+
+            return .success(response)
+        } catch {
+            return .failure(error)
+        }
+    }
+}
