6f17d6a82691c15280d21d618b00c9b1cd140030
Christopher Jr Riley <chrisjr@cjrriley.com>
2024-02-26 21:58:19 -0500

    Add RequestEmailConfirmation

---
+++ Sources/ATProtoKit/Networking/CoreAPI/RequestEmailConfirmation.swift
@@ -0,0 +1,26 @@
+//
+//  RequestEmailConfirmation.swift
+//
+//
+//  Created by Christopher Jr Riley on 2024-02-26.
+//
+
+import Foundation
+
+extension ATProtoKit {
+    /// Requests an email confirmation to verify the email belongs to the user.
+    public func requestEmailConfirmation() async throws {
+        guard let sessionURL = session.pdsURL,
+              let requestURL = URL(string: "\(sessionURL)/xrpc/com.atproto.server.requestEmailConfirmation") else {
+            throw NSError(domain: "", code: -1, userInfo: [NSLocalizedDescriptionKey : "Invalid URL"])
+        }
+
+        do {
+            let request = APIClientService.createRequest(forRequest: requestURL, andMethod: .post, acceptValue: nil, contentTypeValue: nil, authorizationValue: "Bearer \(session.accessToken)")
+            let response = try await APIClientService.sendRequest(request)
+        } catch {
+            throw error
+        }
+    }
+}
+
