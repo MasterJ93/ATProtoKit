4595f13cfa42457c6a5eec600d666b9091ecb3f9
Christopher Jr Riley <chrisjr@cjrriley.com>
2024-02-27 11:20:37 -0500

    Add RevokeAppPassword

---
+++ Sources/ATProtoKit/Models/Lexicons/com.atproto/Server/AtprotoServerRevokeAppPassword.swift
@@ -0,0 +1,20 @@
+//
+//  AtprotoServerRevokeAppPassword.swift
+//
+//
+//  Created by Christopher Jr Riley on 2024-02-27.
+//
+
+import Foundation
+
+/// The main data model definition for revoking a password.
+///
+/// - Note: According to the AT Protocol specifications: "Revoke an App Password by name."
+///
+/// - SeeAlso: This is based on the [`com.atproto.server.revokeAppPassword`][github] lexicon.
+///
+/// [github]: https://github.com/bluesky-social/atproto/blob/main/lexicons/com/atproto/server/revokeAppPassword.json
+public struct ServerRevokeAppPassword: Codable {
+    /// The name associated with the App Password.
+    public let appPasswordName: String
+}
---
+++ Sources/ATProtoKit/Networking/CoreAPI/RevokeAppPassword.swift
@@ -0,0 +1,29 @@
+//
+//  RevokeAppPassword.swift
+//
+//
+//  Created by Christopher Jr Riley on 2024-02-27.
+//
+
+import Foundation
+
+extension ATProtoKit {
+    /// Revokes an App Password from a user's account.
+    /// 
+    /// - Parameter appPasswordName: The name associated with the App Password.
+    public func revokeAppPassword(named appPasswordName: String) async throws {
+        guard let sessionURL = session.pdsURL,
+              let requestURL = URL(string: "\(sessionURL)/xrpc/com.atproto.server.revokeAppPassword") else {
+            throw NSError(domain: "", code: -1, userInfo: [NSLocalizedDescriptionKey : "Invalid URL"])
+        }
+        let requestBody = ServerRevokeAppPassword(appPasswordName: appPasswordName)
+
+        do {
+            let request = APIClientService.createRequest(forRequest: requestURL, andMethod: .post, acceptValue: nil, contentTypeValue: "application/json", authorizationValue: "Bearer \(session.accessToken)")
+
+            try await APIClientService.sendRequest(request, withEncodingBody: requestBody)
+        } catch {
+            throw error
+        }
+    }
+}
