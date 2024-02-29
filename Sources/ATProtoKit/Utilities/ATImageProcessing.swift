b06d2709ae2b4d2856fe972d79fcc53fd8769d63
Christopher Jr Riley <chrisjr@cjrriley.com>
2024-02-28 20:37:57 -0500

    Make changes to ATImageProcessing
    - Added documentation for ATImage
    - Changed the length of each line of documentation so it doesn't
    go over 170 characters per line.
    - Removed some duplicated documentation.
    - Changed convertToImageQuery() from having the "image" parameter
    be of type AnyObject to ATImage.
    - Changed the ImageProcessable protocol name to ATImageProcessable.
    - Add documentation to each of the protocol's methods.

--- Sources/ATProtoKit/Utilities/ATImageProcessing.swift
+++ Sources/ATProtoKit/Utilities/ATImageProcessing.swift
@@ -9,10 +9,18 @@ import Foundation
 
 #if canImport(UIKit)
 import UIKit
-typealias ATImage = UIImage
+/// A type for `UIImage` on platforms where `UIKit` is available (iOS, iPadOS, tvOS, watchOS, visionOS, Mac Catalyst).
+///
+/// `ATImage` is a type alias that gives a unified way of handling images for preparation for uploading to the AT Protocol. This allows the abstraction of both `UIImage` and `NSImage`, giving
+/// `ATImageProcessable` more breathing room for implementation.
+public typealias ATImage = UIImage
 #elseif canImport(AppKit)
 import AppKit
-typealias ATImage = NSImage
+/// A type for `NSImage` on platforms where `AppKit` is available (macOS).
+///
+/// `ATImage` is a type alias that gives a unified way of handling images for preparation for uploading to the AT Protocol. This allows the abstraction of both `UIImage` and `NSImage`, giving
+/// `ATImageProcessable` more breathing room for implementation.
+public typealias ATImage = NSImage
 #endif
 
 protocol ImageProtocol {
@@ -23,42 +31,47 @@ protocol ImageProtocol {
 
 
 /// Provides a standardized approach to processing images for uploading to servers that interact with the AT Protocol.
-///
+/// 
 /// This protocol aids in the conversion of images into an ``ImageQuery`` (which is used to give all of the necessary information
 /// about the image for the server), while ensuring that privacy-sensitive EXIF data is stripped from the images before upload.
-///
-/// Implementing `ATImageProcessable` allows for the customization of image processing tasks, making it useful for developers working on platforms where standard image objects like `UIImage` or `NSImage` are not available, such as in Linux environments.
+/// 
+/// Implementing `ATImageProcessable` allows for the customization of image processing tasks, making it useful for developers working on platforms where standard image objects like
+/// `UIImage` or `NSImage` are not available, such as in Linux environments.
 ///
 /// ### Methods
-///   - `convertToImageQuery(image:, altText:)`: Converts an image object into an `ImageQuery` instance. This instance encapsulates the image data along with metadata such as the file name and alternative text, preparing it for upload.
-///   - `stripEXIFData(from:)`: Removes EXIF metadata from the provided image data. This is crucial for protecting privacy and reducing the size of the image data being transmitted. Please note that this is an important method to create as, according to the AT Protocol documentation, the protocol may be more strict about stripping metadata in the future.
-/// - Important: `stripEXIFData(from data: Data)` is an important method to create as, according to the AT Protocol documentation, the protocol may be more strict about stripping metadata in the future.
+///   - `convertToImageQuery(image:altText:)`: Converts an image object into an `ImageQuery` instance. This instance encapsulates the image data along with metadata such as the file name
+/// and alternative text, preparing it for upload.
+///   - `stripMetadata(from:)`: Removes EXIF and GPS metadata from the provided image data. This is crucial for protecting privacy and reducing the size of the image data being transmitted.
+/// - Important: `stripMetadata(from data:)` is an important method to create as, according to the AT Protocol documentation, the protocol may be more strict about stripping metadata in the future.\
+/// \
+/// Also, this should be an `internal` method, as it will be part of `convertToImageQuery(image:, altText:)`. It's recommended that it's called before ``convertToImageQuery(image:altText:)``
+/// attempts to access the image.
 ///
 /// ### Example
 /// Below is a sample implementation showcasing how to conform to `ATImageProcessable` for a custom image type:
 /// ```swift
 /// class CustomImageProcessor: ATImageProcessable {
-///      func convertToImageQuery(image: AnyObject, altText: String) -> ImageQuery? {
+///      func convertToImageQuery(image: ATImage, altText: String) -> ImageQuery? {
 ///          guard let customImage = image as? CustomImageType else {
 ///              return nil
 ///          }
 ///          let imageData = customImage.convertToData() // Your method to convert the image to Data
 ///          let fileName = "customImage.jpg" // Determine an appropriate file name
-///
+/// 
 ///          // Strip EXIF data if necessary
 ///          let cleanImageData = stripEXIFData(from: imageData)
-///
+/// 
 ///          return ImageQuery(imageData: cleanImageData, fileName: fileName, altText: altText)
 ///      }
-///
+/// 
 ///      func stripEXIFData(from data: Data) -> Data {
 ///          // Implementation to remove EXIF data from the image data.
 ///          return data
 ///      }
 /// }
-///
+/// 
 /// ```
-///
+/// 
 /// ### Usage
 /// After implementing `ATImageProcessable`, you can use your custom image processor to prepare images for upload in a way
 /// that aligns with your application's requirements:
@@ -67,11 +80,21 @@ protocol ImageProtocol {
 ///     let customImageProcessor = CustomImageProcessor()
 ///     let image1 = customImageProcessor.convertToData("path/to/image/cat.jpg", withAltText: "A cat, looking directly at the camera.").stripEXIFData()
 ///     let imageQueries = [image1]
-///
+/// 
 ///     await atProto.createPostRecord(text: "Look at my cute cat!", embed: .images(images: imageQueries))
 /// }
 /// ```
-public protocol ImageProcessable {
-    func convertToImageQuery(image: AnyObject, altText: String) -> ImageQuery?
-    func stripEXIFData(from data: Data) -> Data
+public protocol ATImageProcessable {
+    /// Processes an image to be suitable for the AT Protocol servers.
+    ///
+    /// It's required for the image to be less than 1MB (1,000,000 bytes). This method will shrink the file size to the largest possible size that doesn't exceed the limit. Note that image quality may be affected.
+    /// - Parameters:
+    ///   - image: The image that needs to be prepared.
+    ///   - altText: The alt text used to help blind and low-vision users know what's contained in the text. Optional.
+    /// - Returns: An ``ImageQuery``, which combines the image itself in a `Data` format and the alt text.
+    func convertToImageQuery(image: ATImage, altText: String?) -> ImageQuery?
+    /// Removes all EXIF and GPS metadata from the image.
+    ///
+    /// This method should ideally be before ``convertToImageQuery(image:altText:)`` does anything to the image, as it will help with the process of maintaining more of the image quality.
+    func stripMetadata(from data: ATImage) -> ATImage
 }
