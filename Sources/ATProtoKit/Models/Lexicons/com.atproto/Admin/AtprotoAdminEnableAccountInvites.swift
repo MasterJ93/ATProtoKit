9ac127b7837e33472fb5ed911d7e4890e78c74ad
Christopher Jr Riley <chrisjr@cjrriley.com>
2024-02-29 14:38:10 -0500

    Add EnableAccountInvitesAsAdmin

---
+++ Sources/ATProtoKit/Models/Lexicons/com.atproto/Admin/AtprotoAdminEnableAccountInvites.swift
@@ -0,0 +1,27 @@
+//
+//  AtprotoAdminEnableAccountInvites.swift
+//
+//
+//  Created by Christopher Jr Riley on 2024-02-29.
+//
+
+import Foundation
+
+/// The main data model definition for giving the user account access to receive invite codes again.
+///
+/// - Note: According to the AT Protocol specifications: "Re-enable an account's ability to receive invite codes."
+///
+/// - SeeAlso: This is based on the [`com.atproto.admin.enableAccountInvites`][github] lexicon.
+///
+/// [github]: https://github.com/bluesky-social/atproto/blob/main/lexicons/com/atproto/admin/enableAccountInvites.json
+public struct AdminEnableAccountInvites: Codable {
+    /// The decentralized identifier (DID) of the account that will regain access to receiving invite codes.
+    public let accountDID: String
+    /// A note as to why this action is being done. Optional.
+    public let note: String?
+
+    enum CodingKeys: String, CodingKey {
+        case accountDID = "account"
+        case note
+    }
+}
---
+++ Sources/ATProtoKit/Networking/CoreAPI/EnableAccountInvitesAsAdmin.swift
@@ -0,0 +1,32 @@
+//
+//  EnableAccountInvitesAsAdmin.swift
+//
+//
+//  Created by Christopher Jr Riley on 2024-02-29.
+//
+
+import Foundation
+
+extension ATProtoKit {
+    /// Turns on the ability for the user account to receive invite codes again.
+    ///
+    /// This only works if the user account had lost access to this ability.
+    ///
+    /// - Important: This is an administrator task and as such, regular users won't be able to access this; if they attempt to do so, an error will occur.
+    public func enableAccountInvitesAsAdmin(_ accountDID: String, note: String?) async throws {
+        guard let sessionURL = session.pdsURL,
+              let requestURL = URL(string: "\(sessionURL)/xrpc/com.atproto.admin.disableAccountInvites") else {
+            throw NSError(domain: "", code: -1, userInfo: [NSLocalizedDescriptionKey: "Invalid URL"])
+        }
+
+        let requestBody = AdminEnableAccountInvites(accountDID: accountDID, note: note)
+
+        do {
+            let request = APIClientService.createRequest(forRequest: requestURL, andMethod: .post, acceptValue: nil, contentTypeValue: "application/json", authorizationValue: "Bearer \(session.accessToken)")
+
+            try await APIClientService.sendRequest(request, withEncodingBody: requestBody)
+        } catch {
+            throw error
+        }
+    }
+}
