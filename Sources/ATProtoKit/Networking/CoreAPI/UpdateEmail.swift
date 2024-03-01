e1cc5dac240baf3f1f4602dd96247d337866bd3c
Christopher Jr Riley <chrisjr@cjrriley.com>
2024-02-29 20:11:35 -0500

    Add more documentation to updateEmail

--- Sources/ATProtoKit/Networking/CoreAPI/UpdateEmail.swift
+++ Sources/ATProtoKit/Networking/CoreAPI/UpdateEmail.swift
@@ -9,7 +9,12 @@ import Foundation
 
 extension ATProtoKit {
     /// Updates the email address associated with the user's account.
+    /// 
+    /// - Important: If the email has already been confirmed, then `token` must be used. `token` can be retrieved by sending an email to the confirmed email address using ``requestEmailUpdate()``.
     ///
+    /// - Parameters:
+    ///   - email: The new email addtess the user wants to associate with their account.
+    ///   - token: The token used to confirm the change. Optional.
     public func updateEmail(_ email: String, token: String? = nil) async throws {
         guard let sessionURL = session.pdsURL,
               let requestURL = URL(string: "\(sessionURL)/xrpc/om.atproto.server.updateEmail") else {
