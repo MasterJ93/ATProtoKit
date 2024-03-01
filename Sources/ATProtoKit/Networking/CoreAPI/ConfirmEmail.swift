c3727ce7c8186c8bad472c7c6874b74713762f48
Christopher Jr Riley <chrisjr@cjrriley.com>
2024-02-29 20:24:20 -0500

    Add more docs to requestEmailConfirmation

--- Sources/ATProtoKit/Networking/CoreAPI/ConfirmEmail.swift
+++ Sources/ATProtoKit/Networking/CoreAPI/ConfirmEmail.swift
@@ -11,6 +11,12 @@ extension ATProtoKit {
     /// Confirms an email by the user using the token that was sent to the user's email inbox.
     /// 
     /// The token in the email comes from the result of ``requestEmailConfirmation``. The token is simply sent to the user's email: it doesn't appear as the output of that method's response.
+    /// 
+    /// - Important: `token` is required. Getting `token` requires an email to be sent to the email address. You can send this email via ``requestEmailConfirmation()``.
+    ///
+    /// - Parameters:
+    ///   - email: The email address to confirm.
+    ///   - token: The token used to confirm the email address.
     public func confirmEmail(_ email: String, token: String) async throws {
         guard let sessionURL = session.pdsURL,
               let requestURL = URL(string: "\(sessionURL)/xrpc/com.atproto.server.confirmEmail") else {
