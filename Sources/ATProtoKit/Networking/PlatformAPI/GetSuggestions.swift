e65267f138bedca496a6a0fedf36a91a74c07229
Christopher Jr Riley <chrisjr@cjrriley.com>
2024-02-29 23:55:37 -0500

    Add GetInviteCodesAsAdmin and fix docs
    A link to ActorGetSuggestionsOutput was added in
    getSuggestions.

--- Sources/ATProtoKit/Networking/PlatformAPI/GetSuggestions.swift
+++ Sources/ATProtoKit/Networking/PlatformAPI/GetSuggestions.swift
@@ -13,7 +13,7 @@ extension ATProtoKit {
     /// - Parameters:
     ///   - limit: The number of suggested users to follow. Optional. Defaults to 50. Can only choose between 1 and 100.
     ///   - cursor: The mark used to indicate the starting point for the next set of results. Optional.
-    /// - Returns: A `Result`, containing either an `ActorGetSuggestionsOutput` if succesful, or an `Error` if it's not.
+    /// - Returns: A `Result`, containing either an ``ActorGetSuggestionsOutput`` if succesful, or an `Error` if it's not.
     public func getSuggestions(_ limit: Int? = 50, cursor: String? = nil) async throws -> Result<ActorGetSuggestionsOutput, Error> {
         guard let sessionURL = session.pdsURL,
               let requestURL = URL(string: "\(sessionURL)/xrpc/app.bsky.actor.getSuggestions") else {
