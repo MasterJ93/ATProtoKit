a13568d68f4aa30f5243ea78b9ed0aed48bb2c20
Christopher Jr Riley <chrisjr@cjrriley.com>
2024-03-03 01:42:20 -0500

    Add UpdateCommunicationTemplate

---
+++ Sources/ATProtoKit/Models/Lexicons/com.atproto/Admin/AtprotoAdminUpdateCommunicationTemplate.swift
@@ -0,0 +1,41 @@
+//
+//  AtprotoAdminUpdateCommunicationTemplate.swift
+//
+//
+//  Created by Christopher Jr Riley on 2024-03-03.
+//
+
+import Foundation
+
+/// The main data model definition for updating a communication template.
+///
+/// - Note: According to the AT Protocol specifications: "Administrative action to update an existing communication template. Allows passing partial fields to patch specific fields only."
+///
+/// - SeeAlso: This is based on the [`com.atproto.admin.updateCommunicationTemplate`][github] lexicon.
+///
+/// [github]: https://github.com/bluesky-social/atproto/blob/main/lexicons/com/atproto/admin/updateCommunicationTemplate.json
+public struct AdminUpdateCommunicationTemplate: Codable {
+    /// The ID of the communication template.
+    public let id: String
+    /// The name of the communication template. Optional.
+    public let name: String?
+    /// The content of the communication template. Optional.
+    ///
+    /// This may contain Markdown placeholders and variable placeholders.
+    public let contentMarkdown: String?
+    /// The subject line of the message itself. Optional.
+    public let subject: String?
+    /// The decentralized identifier (DID) of the user who updated the communication template. Optional.
+    public let updatedBy: String?
+    /// Indicates whether the communication template is disabled. Optional.
+    public let isDisabled: Bool?
+
+    enum CodingKeys: String, CodingKey {
+        case id
+        case name
+        case contentMarkdown
+        case subject
+        case updatedBy
+        case isDisabled = "disabled"
+    }
+}
