1b2b35b884e37bc9f157fb422192d014bcbef954
Christopher Jr Riley <chrisjr@cjrriley.com>
2024-03-03 21:55:45 -0500

    Implement ATProtoKitConfiguration

--- Sources/ATProtoKit/ATProtoKit.swift
+++ Sources/ATProtoKit/ATProtoKit.swift
@@ -1,5 +1,14 @@
 import Foundation
 
+/// Defines a protocol for configurations in the `ATProtoKit` API library.
+///
+/// `ATProtoKitConfiguration` defines the basic requirements for any configuration class or structure
+/// within `ATProtoKit`. Any class that conforms to this protocol must be geared for sending API calls to the AT Protocol. Creating a class that conforms to this is useful if you have additional
+/// lexicons specific to the service you're running.
+public protocol ATProtoKitConfiguration {
+    var session: UserSession { get }
+}
+
 /// The base class that handles the main functionality of the `ATProtoKit` API library.
 ///
 /// For methods which require authentication using an access token, instantiating `ATProtoKit` is required. To get the access token,
@@ -22,7 +31,7 @@ import Foundation
 ///     }
 /// }
 /// ```
-public class ATProtoKit {
+public class ATProtoKit: ATProtoKitConfiguration {
     /// Represents an authenticated user session within the AT Protocol.
     public let session: UserSession
 
@@ -48,3 +57,37 @@ public class ATProtoKit {
         }
     }
 }
+
+/// A class containing all administrator and moderator functionality of the `ATProtoKit` API library.
+///
+/// `ATProtoAdmin` works similarly to the ``ATProtoKit/ATProtoKit`` class, but dedicated for API calls related for administrators and moderators. More specifically, API calls that
+/// work with the `com.atproto.admin.*` and `com.atproto.ozone.*` lexicons.
+///
+/// Instantiating `ATProtoAdmin` is required to use any of the methods. To get the access token, an instance of ``ATProtocolConfiguration`` is required:
+///
+/// ```swift
+/// let config = ATProtocolConfiguration(handle: "example.bsky.social", appPassword: "hunter2")
+/// ```
+/// ``ATProtocolConfiguration/authenticate()`` should then be used to get information about the session. The result is handed over to the `ATProtoAdmin`'s instance:
+///
+///```swift
+/// Task {
+///     let result = try await config.authenticate()
+///
+///     switch result {
+///         case .success(let result):
+///             print("Access token: \(result.accessToken)")
+///         case .failure(let error):
+///             print("Error: \(error)")
+///     }
+/// }
+/// ```
+public class ATProtoAdmin: ATProtoKitConfiguration {
+    public let session: UserSession
+
+    public init(session: UserSession) {
+        self.session = session
+    }
+}
+
+
