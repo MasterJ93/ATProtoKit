e3dfbf34685940b0dd4c5a2ad1173196f98f3131
Christopher Jr Riley <chrisjr@cjrriley.com>
2024-03-10 04:04:04 -0400

    Add GetMutes and GetRelationships

---
+++ Sources/ATProtoKit/Models/Lexicons/app.bsky/Graph/BskyGraphGetMutes.swift
@@ -0,0 +1,22 @@
+//
+//  BskyGraphGetMutes.swift
+//
+//
+//  Created by Christopher Jr Riley on 2024-03-10.
+//
+
+import Foundation
+
+/// The main data model definition for the output of retrieving all accounts the user account is currently muting.
+///
+/// - Note: According to the AT Protocol specifications: "Enumerates accounts that the requesting account (actor) currently has muted. Requires auth."
+///
+/// - SeeAlso: This is based on the [`app.bsky.graph.getMutes`][github] lexicon.
+///
+/// [github]: https://github.com/bluesky-social/atproto/blob/main/lexicons/app/bsky/graph/getMutes.json
+public struct GraphGetMutesOutput: Codable {
+    /// The mark used to indicate the starting point for the next set of results. Optional.
+    public let cursor: String?
+    /// An array of accounts the user account is muting.
+    public let mutes: [ActorProfileView]
+}
