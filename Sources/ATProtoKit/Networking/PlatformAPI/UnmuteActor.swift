854c8933a428cc65d0f0e82ea3671f21fa2730f9
Christopher Jr Riley <chrisjr@cjrriley.com>
2024-03-10 06:37:36 -0400

    Add UnmuteActor

---
+++ Sources/ATProtoKit/Networking/PlatformAPI/UnmuteActor.swift
@@ -0,0 +1,34 @@
+//
+//  UnmuteActor.swift
+//
+//
+//  Created by Christopher Jr Riley on 2024-03-10.
+//
+
+import Foundation
+
+extension ATProtoKit {
+    /// Unmutes a user account.
+    /// 
+    /// - Parameter actorDID: The decentralized identifier (DID) or handle of a user account.
+    public func unmuteActor(_ actorDID: String) async throws {
+        guard let sessionURL = session.pdsURL,
+              let requestURL = URL(string: "\(sessionURL)/xrpc/app.bsky.graph.unmuteActor") else {
+            throw NSError(domain: "", code: -1, userInfo: [NSLocalizedDescriptionKey: "Invalid URL"])
+        }
+
+        let requestBody = GraphUnmuteActor(actorDID: actorDID)
+
+        do {
+            let request = APIClientService.createRequest(forRequest: requestURL,
+                                                         andMethod: .post,
+                                                         acceptValue: "application/json",
+                                                         contentTypeValue: nil,
+                                                         authorizationValue: "Bearer \(session.accessToken)")
+
+            let response = try await APIClientService.sendRequest(request)
+        } catch {
+            throw error
+        }
+    }
+}
