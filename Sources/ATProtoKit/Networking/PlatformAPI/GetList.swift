538b21f287de27a4be773ffb271eaebe213ccc87
Christopher Jr Riley <chrisjr@cjrriley.com>
2024-03-09 23:59:20 -0500

    Add GetList

---
+++ Sources/ATProtoKit/Networking/PlatformAPI/GetList.swift
@@ -0,0 +1,50 @@
+//
+//  GetList.swift
+//
+//
+//  Created by Christopher Jr Riley on 2024-03-09.
+//
+
+import Foundation
+
+extension ATProtoKit {
+    /// Grabs a given list.
+    ///
+    public func getList(from listURI: String, limit: Int? = 50, cursor: String? = nil) async throws -> Result<GraphGetListOutput, Error> {
+        guard let sessionURL = session.pdsURL,
+              let requestURL = URL(string: "\(sessionURL)/xrpc/app.bsky.graph.getList") else {
+            return .failure(NSError(domain: "", code: -1, userInfo: [NSLocalizedDescriptionKey: "Invalid URL"]))
+        }
+
+        var queryItems = [(String, String)]()
+
+        queryItems.append(("list", listURI))
+
+        if let limit {
+            let finalLimit = min(1, max(limit, 100))
+            queryItems.append(("limit", "\(finalLimit)"))
+        }
+
+        if let cursor {
+            queryItems.append(("cursor", cursor))
+        }
+
+        do {
+            let queryURL = try APIClientService.setQueryItems(
+                for: requestURL,
+                with: queryItems
+            )
+
+            let request = APIClientService.createRequest(forRequest: queryURL,
+                                                         andMethod: .get,
+                                                         acceptValue: "application/json",
+                                                         contentTypeValue: nil,
+                                                         authorizationValue: "Bearer \(session.accessToken)")
+            let response = try await APIClientService.sendRequest(request, decodeTo: GraphGetListOutput.self)
+
+            return .success(response)
+        } catch {
+            return .failure(error)
+        }
+    }
+}
