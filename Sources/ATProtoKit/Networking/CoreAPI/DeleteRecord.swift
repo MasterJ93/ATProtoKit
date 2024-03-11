2cab1748c722da19eefa10f474e1fcdd0f9befec
Christopher Jr Riley <chrisjr@cjrriley.com>
2024-03-11 11:10:11 -0400

    Add RepoDeleteRecord and tweak methods
    deleteRecord already exists due to the existence of
    deleteLikeRecord. This commit adds a new request body struct
    that aligns the method more closely with the design of the
    rest of the library. deleteLikeRecord has also been tweaked to
    reflect the changes made by deleteRecord.

--- Sources/ATProtoKit/Networking/CoreAPI/DeleteRecord.swift
+++ Sources/ATProtoKit/Networking/CoreAPI/DeleteRecord.swift
@@ -8,21 +8,35 @@
 import Foundation
 
 extension ATProtoKit {
-    /// Deletes a record.
-    ///
-    /// - Note: According to the AT Protocol specifications: "Delete a repository record, or ensure it doesn't exist. Requires auth, implemented by PDS."
+    /// Deletes a record attached to a user account..
     /// 
+    /// - Note: According to the AT Protocol specifications: "Delete a repository record, or ensure it doesn't exist. Requires auth, implemented by PDS."
+    ///  
     /// - Parameters:
-    ///   - requestBody: The request body that contains the specific record that needs to be deleted.
-    public func deleteRecord<T: Encodable>(requestBody: T) async throws {
+    ///   - repositoryDID: The decentralized identifier (DID) or handle of the user account.
+    ///   - collection: The NSID of the record.
+    ///   - recordKey: The record key of the record.
+    ///   - swapRecord: Swap the record on the server with this current record based on the CID of the record on the server.
+    ///   - swapCommit: Swap the commit on the server with this current commit based on the CID of the commit on the server.
+    public func deleteRecord(repositoryDID: String, collection: String, recordKey: String, swapRecord: String? = nil, swapCommit: String? = nil) async throws {
         guard let sessionURL = session.pdsURL,
               let requestURL = URL(string: "\(sessionURL)/xrpc/com.atproto.repo.deleteRecord") else {
             throw NSError(domain: "", code: -1, userInfo: [NSLocalizedDescriptionKey: "Invalid URL"])
         }
 
+        let requestBody = RepoDeleteRecord(
+            repositoryDID: repositoryDID,
+            collection: collection,
+            recordKey: recordKey,
+            swapRecord: swapRecord,
+            swapCommit: swapCommit
+        )
+
         do {
             let request = APIClientService.createRequest(forRequest: requestURL,
                                                          andMethod: .post,
+                                                         acceptValue: nil,
+                                                         contentTypeValue: "application/json",
                                                          authorizationValue: "Bearer \(session.accessToken)")
 
             try await APIClientService.sendRequest(request, withEncodingBody: requestBody)
