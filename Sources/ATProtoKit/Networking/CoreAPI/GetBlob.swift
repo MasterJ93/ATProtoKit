2dc25dbe0bf5e55e5931d7f61ac28aca7db6c49d
Christopher Jr Riley <chrisjr@cjrriley.com>
2024-03-12 21:25:05 -0400

    Require pdsURL in GetBlocks

--- Sources/ATProtoKit/Networking/CoreAPI/GetBlob.swift
+++ Sources/ATProtoKit/Networking/CoreAPI/GetBlob.swift
@@ -12,9 +12,8 @@ extension ATProtoKit {
     ///
     /// - Parameter blobQuery: An object containing the `atDID` and `cidHash` of the blob.
     /// - Returns: A `Result` containing a `Data` object on success or an `Error` on failure.
-    public static func getBlob(from blobQuery: BlobQuery, pdsURL: String? = "https://bsky.social") async -> Result<Data, Error> {
-        guard let sessionURL = pdsURL,
-              let requestURL = URL(string: "\(sessionURL)/xrpc/com.atproto.sync.getBlob") else {
+    public static func getBlob(from blobQuery: BlobQuery, pdsURL: String = "https://bsky.social") async -> Result<Data, Error> {
+        guard let requestURL = URL(string: "\(pdsURL)/xrpc/com.atproto.sync.getBlob") else {
             print("Failure")
             return .failure(URIError.invalidFormat)
         }
