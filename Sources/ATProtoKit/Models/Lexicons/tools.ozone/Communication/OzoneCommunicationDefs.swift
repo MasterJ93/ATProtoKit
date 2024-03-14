0c50b0ca5db3c98c4511f8800ed21d79d8039ca3
Christopher Jr Riley <chrisjr@cjrriley.com>
2024-03-14 08:16:36 -0400

    Add def files for ozone and move CreateReport

---
+++ Sources/ATProtoKit/Models/Lexicons/tools.ozone/Communication/OzoneCommunicationDefs.swift
@@ -0,0 +1,8 @@
+//
+//  OzoneCommunicationDefs.swift
+//
+//
+//  Created by Christopher Jr Riley on 2024-03-14.
+//
+
+import Foundation
---
+++ Sources/ATProtoKit/Models/Lexicons/tools.ozone/Moderation/OzoneModerationDefs.swift
@@ -0,0 +1,8 @@
+//
+//  OzoneModerationDefs.swift
+//
+//
+//  Created by Christopher Jr Riley on 2024-03-14.
+//
+
+import Foundation
