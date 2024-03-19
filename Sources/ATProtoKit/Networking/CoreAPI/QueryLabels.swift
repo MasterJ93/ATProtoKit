f77be8495e1dcfe924ea784ce8f9da51f401fd85
Christopher Jr Riley <chrisjr@cjrriley.com>
2024-03-19 03:07:36 -0400

    Conform label methods to guidelines
    The following changes were made:
    - A note of the lexicon’s official description is added.
    - The link and namespaced identifier of the lexicon is added.
    - Expanded “NSID” to “Namespaced Identifier (NSID)”.
    - Made sure the Result field is filled.
    - Remove the static keyword.
    - For methods where authentication isn’t used, pdsURL is now an
    optional and the default value is set to nil.
    - For methods where authentication isn’t required, accessToken
    is now shouldAuthenticate, which is of type Bool and is set to false.
    - For methods where authentication isn’t used, there will be a
    sessionURL variable that is set to pdsURL (if the parameter is not
    nil), or session.pdsURL (if the parameter is nil).
    - For methods where authentication isn’t required, an additional
    accessToken variable is used, returning nil (if shouldAuthenticate
    is false) or “Bearer \(session.accessToken)” (if it returns true).
    - The guard statement now throws
    ATRequestPrepareError.invalidRequestURL.
    - All requestBody parameters will have their own separate line.
    - queryURL will be declared outside of the do-catch blocks.
    - APIClientService.sendRequest()’s parameters will have their own
    separate line, unless it only has one.
    
    This commit also renames queryLabel to queryLabels and
    LabelQueryLabelOutput to LabelQueryLabelsOutput.

--- Sources/ATProtoKit/Networking/CoreAPI/QueryLabels.swift
+++ Sources/ATProtoKit/Networking/CoreAPI/QueryLabels.swift
@@ -1,5 +1,5 @@
 //
-//  QueryLabel.swift
+//  QueryLabels.swift
 //
 //
 //  Created by Christopher Jr Riley on 2024-03-16.
@@ -12,19 +12,34 @@ extension ATProtoKit {
     ///
     /// `uriPatterns` will match with the boolean "OR". Each URI pattern can either have the `*` prefix or the full URI.
     ///
+    /// - Note: According to the AT Protocol specifications: "Find labels relevant to the provided AT-URI patterns. Public endpoint for moderation services, though may return different or additional results with auth."
+    ///
+    /// - SeeAlso: This is based on the [`com.atproto.label.queryLabels`][github] lexicon.
+    ///
+    /// [github]: https://github.com/bluesky-social/atproto/blob/main/lexicons/com/atproto/label/queryLabels.json
+    ///
     /// - Parameters:
     ///   - uriPatterns: An array of URI patterns.
     ///   - sources: An array of decentralized identifiers (DIDs) for label sources. Optional.
     ///   - limit: The number of suggested users to follow. Optional. Defaults to `50`. Can only choose between `1` and `250`.
     ///   - cursor: The mark used to indicate the starting point for the next set of results. Optional.
-    ///   - isAuthenticated: Indicates whether the method will use the access token when sending the request. Defaults to `false`.
+    ///   - pdsURL: The URL of the Personal Data Server (PDS). Defaults to `nil`.
+    ///   - shouldAuthenticate: Indicates whether the method will use the access token when sending the request. Defaults to `false`.
     /// - Returns: A `Result`, containing either a ``LabelQueryLabelOutput`` if successful, ot an `Error` if not.
-    public func queryLabel(uriPatterns: [String], sources: [String]?, limit: Int? = 50, cursor: String? = nil, isAuthenticated: Bool = false) async throws -> Result<LabelQueryLabelOutput, Error> {
-        let accessToken = isAuthenticated == true ? "Bearer \(session.accessToken)" : nil
+    public func queryLabels(uriPatterns: [String], sources: [String]?, limit: Int? = 50, cursor: String? = nil, pdsURL: String? = nil,
+                            shouldAuthenticate: Bool = false) async throws -> Result<LabelQueryLabelsOutput, Error> {
+        var accessToken: String? = nil
 
-        guard let sessionURL = session.pdsURL,
+        if shouldAuthenticate == true {
+            guard session != nil,
+                  accessToken == session?.accessToken else {
+                throw ATRequestPrepareError.missingActiveSession
+            }
+        }
+
+        guard let sessionURL = pdsURL != nil ? pdsURL : session?.pdsURL,
               let requestURL = URL(string: "\(sessionURL)/xrpc/com.atproto.label.queryLabels") else {
-            throw NSError(domain: "", code: -1, userInfo: [NSLocalizedDescriptionKey: "Invalid URL"])
+            throw ATRequestPrepareError.missingActiveSession
         }
 
         var queryItems = [(String, String)]()
@@ -55,7 +70,8 @@ extension ATProtoKit {
                                                          acceptValue: "application/json",
                                                          contentTypeValue: nil,
                                                          authorizationValue: accessToken)
-            let response = try await APIClientService.sendRequest(request, decodeTo: LabelQueryLabelOutput.self)
+            let response = try await APIClientService.sendRequest(request,
+                                                                  decodeTo: LabelQueryLabelsOutput.self)
 
             return .success(response)
         } catch {
