dee8f29f7f992c32cd68c3fea3db711b082f0c8d
Christopher Jr Riley <chrisjr@cjrriley.com>
2024-03-27 05:01:27 -0400

    Fix grammar error in docs

--- Sources/ATProtoKit/Models/Common/ATEventStreamConfiguration/ATEventStreamModels.swift
+++ Sources/ATProtoKit/Models/Common/ATEventStreamConfiguration/ATEventStreamModels.swift
@@ -36,7 +36,7 @@ public protocol ATEventStreamConfiguration: Decodable {
     /// Normally, when connecting to the event stream, it will start from the first message the event stream gets. The client will always look at the last successful
     /// `sequencePosition` and stores it internally. However, the following can occur when `cursor` is invloved:
     /// - If `cursor` is higher than `sequencePosition`, the connection will close after outputting an error.
-    /// - If `cursor`is within the server's rollback window, the server will attempt to give the client all of the messages it might have missed.
+    /// - If `cursor` is within the server's rollback window, the server will attempt to give the client all of the messages it might have missed.
     /// - If `cursor` is outside of the rollback window, then the server will send an info message saying it's too old, then sends the oldest message it has and
     /// continues the stream.
     /// - If `cursor` is `0`, then the server will send the oldest message it has and continues the stream.
