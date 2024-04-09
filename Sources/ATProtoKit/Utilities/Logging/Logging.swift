4536b1de20968aab329e0f1c4ece5b236c91041e
Christopher Jr Riley <chrisjr@cjrriley.com>
2024-04-09 11:56:45 -0400

    Comment out logging
    This code isn't working, so it'll be commented out until
    a better solution arrives. For now, a Swift package already
    works, but the ideal thing is to have one less dependency
    from logs (using `SwiftLog`, however, is unavoidable).

--- Sources/ATProtoKit/Utilities/Logging/Logging.swift
+++ Sources/ATProtoKit/Utilities/Logging/Logging.swift
@@ -7,51 +7,49 @@
 
 import Logging
 
-#if canImport(os)
-import os
-
-struct OSLogHandler: LogHandler {
-    public let subsystem: String
-    public let category: String
-    public var logLevel: Logging.Logger.Level = .info
-    public var metadata: Logging.Logger.Metadata = [:]
-    private let appleLogger: Logging.Logger
-
-    init(subsystem: String, category: String) {
-        self.subsystem = subsystem
-        self.category = category
-    }
-
-    public func log(level: Logging.Logger.Level, message: Logging.Logger.Message, metadata: Logging.Logger.Metadata?, source: String, file: String,
-                    function: String, line: UInt) {
-        let combinedMetadata = (metadata ?? [:]).merging(self.metadata) { _, new in new }
-        let metaString = combinedMetadata.map { "\($0)=\($1)" }.joined(separator: ", ")
-        let logMessage = "\(message) \(metaString)"
-
-        let appleLogger = Logger(subsystem: subsystem, category: category)
-
-        switch level {
-            case .trace:
-                appleLogger.trace("%{public}@")
-            case .debug:
-                appleLogger.debug("%{public}@")
-            case .info:
-                appleLogger.info("%{public}@")
-            case .notice:
-                appleLogger.notice("%{public}@")
-            case .warning:
-                appleLogger.warning("%{public}@")
-            case .error:
-                appleLogger.error("%{public}@")
-            case .critical:
-                appleLogger.critical("%{public}@")
-        }
-    }
-
-    subscript(metadataKey key: String) -> Logging.Logger.Metadata.Value? {
-        get { metadata[key] }
-        set { metadata[key] = newValue }
-    }
-}
-#endif
-
+//#if canImport(os)
+//import os
+//
+//struct OSLogHandler: LogHandler {
+//    public let subsystem: String
+//    public let category: String
+//    public var logLevel: Logging.Logger.Level = .info
+//    public var metadata: Logging.Logger.Metadata = [:]
+//
+//    init(subsystem: String, category: String) {
+//        self.subsystem = subsystem
+//        self.category = category
+//    }
+//
+//    mutating func log(level: Logging.Logger.Level, message: Logging.Logger.Message, metadata: Logging.Logger.Metadata?, source: String, file: String, function: String, line: UInt) {
+//        let allMetadata = self.metadata.merging(metadata ?? [:]) { _, new in new }
+//        var messageMetadata = [String: Any]()
+//        var privacySettings = [String: OSLogPrivacy]()
+//
+//        let appleLogger = os.Logger(subsystem: subsystem, category: category)
+////        appleLogger(level: level, message: formattedMessage)
+////        appleLogger.log(level: .info, "\(formattedMessage)")
+//        switch level {
+//            case .trace:
+//                appleLogger.trace("\(message, privacy: .public)")
+//            case .debug:
+//                appleLogger.debug("")
+//            case .info:
+//                appleLogger.info("%{public}@")
+//            case .notice:
+//                appleLogger.notice("%{public}@")
+//            case .warning:
+//                appleLogger.warning("%{public}@")
+//            case .error:
+//                appleLogger.error("%{public}@")
+//            case .critical:
+//                appleLogger.critical("%{public}@")
+//        }
+//    }
+//
+//    subscript(metadataKey key: String) -> Logging.Logger.Metadata.Value? {
+//        get { metadata[key] }
+//        set { metadata[key] = newValue }
+//    }
+//}
+//#endif
