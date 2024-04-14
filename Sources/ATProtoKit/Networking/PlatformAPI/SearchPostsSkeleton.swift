51b6f3aecc49f811a131154bed0aee43c1f54512
Christopher Jr Riley <chrisjr@cjrriley.com>
2024-04-14 19:02:35 -0400

    Update searchPostsSkeleton for latest changes
    Also fixed the type for UnspeccedSearchPostsSkeletonOutput.posts.

--- Sources/ATProtoKit/Networking/PlatformAPI/SearchPostsSkeleton.swift
+++ Sources/ATProtoKit/Networking/PlatformAPI/SearchPostsSkeleton.swift
@@ -20,11 +20,22 @@ extension ATProtoKit {
     /// [github]: https://github.com/bluesky-social/atproto/blob/main/lexicons/app/bsky/unspecced/searchPostsSkeleton.json
     ///
     /// - Parameters:
-    ///   - query: The string used for searching the users.
+    ///   - searchQuery: The string used for searching the users.
+    ///   - sortRanking: The ranking order for the results. Optional. Defaults to `.latest`.
+    ///   - sinceDate: The date and time of the results of posts created after this time. Optional.
+    ///   - untilDate: The date and time of the results of posts created before this time. Optional.
+    ///   - mentionIdentifier: The AT Identifier to filter posts that contains a given user. Optional.
+    ///   - author: Filters posts that were created by the author the AT Identifier resolves to. Optional.
+    ///   - language: Filters posts that have a specific language. Optional.
+    ///   - domain: Filters result to posts containing the facet and embed links that point to a specific domain. Optional.
+    ///   - url: Filters result to posts containing facet and embed links that point to this URL. Optional.
+    ///   - tags: An array of tags to be used against the results. Optional.
     ///   - limit: The number of items that can be in the list. Optional. Defaults to `25`.
     ///   - cursor: The mark used to indicate the starting point for the next set of result. Optional.
     /// - Returns: A `Result`, containing either an ``UnspeccedSearchPostsSkeletonOutput`` if successful, or an `Error` if not.
-    public func searchPostsSkeleton(with searchQuery: String, limit: Int? = 25, cursor: String? = nil,
+    public func searchPostsSkeleton(with searchQuery: String, sortRanking: UnspeccedSearchPostsSortRanking? = .latest, sinceDate: Date?, untilDate: Date?,
+                                    mentionIdentifier: String? = nil, author: String? = nil, language: Locale?, domain: String?, url: String?,
+                                    tags: [String]?, limit: Int? = 25, cursor: String? = nil,
                                     pdsURL: String? = nil) async throws -> Result<UnspeccedSearchPostsSkeletonOutput, Error> {
         guard let sessionURL = pdsURL != nil ? pdsURL : session?.pdsURL,
               let requestURL = URL(string: "\(sessionURL)/xrpc/app.bsky.unspecced.searchPostsSkeleton") else {
@@ -34,6 +45,42 @@ extension ATProtoKit {
         var queryItems = [(String, String)]()
 
         queryItems.append(("q", searchQuery))
+
+        if let sortRanking {
+            queryItems.append(("sort", "\(sortRanking.rawValue)"))
+        }
+
+        if let createdAfterDate = sinceDate, let formattedCreatedAfter = CustomDateFormatter.shared.string(from: createdAfterDate) {
+            queryItems.append(("since", formattedCreatedAfter))
+        }
+
+        if let createdBeforeDate = untilDate, let formattedCreatedBefore = CustomDateFormatter.shared.string(from: createdBeforeDate) {
+            queryItems.append(("until", formattedCreatedBefore))
+        }
+
+        if let mentionIdentifier {
+            queryItems.append(("mentions", mentionIdentifier))
+        }
+
+        if let author {
+            queryItems.append(("author", author))
+        }
+
+        if let language {
+            queryItems.append(("lang", language.identifier))
+        }
+
+        if let domain {
+            queryItems.append(("domain", domain))
+        }
+
+        if let url {
+            queryItems.append(("url", url))
+        }
+
+        if let tags {
+            queryItems += tags.map { ("tag", $0) }
+        }
 
         if let limit {
             let finalLimit = max(1, min(limit, 100))
