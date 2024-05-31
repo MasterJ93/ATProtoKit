17e1b1cfa8b1303aa06fe399cb65322d8b625f36
Christopher Jr Riley <chrisjr@cjrriley.com>
2024-05-31 03:57:56 -0400

    Add ChatGetMessages

---
+++ Sources/ATProtoKit/Networking/ChatAPI/ChatGetMessages.swift
@@ -0,0 +1,62 @@
+//
+//  ChatGetMessages.swift
+//
+//
+//  Created by Christopher Jr Riley on 2024-05-31.
+//
+
+import Foundation
+
+extension ATProtoBlueskyChat {
+
+    /// Retrieves messages from a conversation.
+    ///
+    /// - SeeAlso: This is based on the [`chat.bsky.convo.getMessages`][github] lexicon.
+    ///
+    /// [github]: https://github.com/bluesky-social/atproto/blob/main/lexicons/chat/bsky/convo/getMessages.json
+    ///
+    public func getMessages(
+        from conversationID: String,
+        limit: Int? = 50
+    ) async throws -> Result<ChatBskyLexicon.Conversation.GetMessagesOutput, Error> {
+        guard session != nil,
+              let accessToken = session?.accessToken else {
+            return .failure(ATRequestPrepareError.missingActiveSession)
+        }
+
+        guard let sessionURL = session?.pdsURL,
+              let requestURL = URL(string: "\(sessionURL)/xrpc/chat.bsky.convo.getMessages") else {
+            return .failure(ATRequestPrepareError.invalidRequestURL)
+        }
+
+        var queryItems = [(String, String)]()
+
+        queryItems.append(("convoId", conversationID))
+
+        if let limit {
+            let finalLimit = max(1, min(limit, 100))
+            queryItems.append(("limit", "\(finalLimit)"))
+        }
+
+        let queryURL: URL
+
+        do {
+            queryURL = try APIClientService.setQueryItems(
+                for: requestURL,
+                with: queryItems
+            )
+
+            let request = APIClientService.createRequest(forRequest: queryURL,
+                                                         andMethod: .get,
+                                                         acceptValue: "application/json",
+                                                         contentTypeValue: nil,
+                                                         authorizationValue: "Bearer \(accessToken)")
+            let response = try await APIClientService.sendRequest(request,
+                                                                  decodeTo: ChatBskyLexicon.Conversation.GetMessagesOutput.self)
+
+            return .success(response)
+        } catch {
+            return .failure(error)
+        }
+    }
+}
