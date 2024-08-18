2e9190bc02dd34dd596752f3a64a97c6b1d10e83
Christopher Jr Riley <chrisjr@cjrriley.com>
2024-08-18 09:30:06 -0400

    Move methods to seprate files
    This moves all methods (expect the init method) in
    ATProtocolConfiguration into separate files. They are
    still part of the class, but inside extensions.

---
+++ Sources/ATProtoKit/APIReference/SessionManager/APIReference/RefreshSession.swift
@@ -0,0 +1,57 @@
+//
+//  RefreshSession.swift
+//
+//
+//  Created by Christopher Jr Riley on 2024-08-18.
+//
+
+import Foundation
+
+extension ATProtocolConfiguration {
+
+    /// Refreshes the user's session using a refresh token.
+    ///
+    /// - Note: According to the AT Protocol specifications: "Refresh an authentication session.
+    /// Requires auth using the 'refreshJwt' (not the 'accessJwt')."
+    ///
+    /// - SeeAlso: This is based on the [`com.atproto.server.refreshSession`][github] lexicon.
+    ///
+    /// [github]: https://github.com/bluesky-social/atproto/blob/main/lexicons/com/atproto/server/refreshSession.json
+    ///
+    /// - Parameters:
+    ///   - refreshToken: The refresh token for the session.
+    ///   - pdsURL: The URL of the Personal Data Server (PDS). Defaults to `nil`.
+    /// - Returns: An instance of an authenticated user session within the AT Protocol. It may also
+    /// have logging information, as well as the URL of the Personal Data Server (PDS).
+    ///
+    /// - Throws: An ``ATProtoError``-conforming error type, depending on the issue. Go to
+    /// ``ATAPIError`` and ``ATRequestPrepareError`` for more details.
+    public func refreshSession(
+        using refreshToken: String,
+        pdsURL: String? = nil
+    ) async throws -> UserSession {
+        guard let sessionURL = pdsURL != nil ? pdsURL : self.pdsURL,
+              let requestURL = URL(string: "\(sessionURL)/xrpc/com.atproto.server.refreshSession") else {
+            throw ATRequestPrepareError.invalidRequestURL
+        }
+
+        do {
+            let request = APIClientService.createRequest(forRequest: requestURL,
+                                                         andMethod: .post,
+                                                         authorizationValue: "Bearer \(refreshToken)")
+            var response = try await APIClientService.shared.sendRequest(
+                request,
+                decodeTo: UserSession.self
+            )
+            response.pdsURL = self.pdsURL
+
+            if self.logger != nil {
+                response.logger = self.logger
+            }
+
+            return response
+        } catch {
+            throw error
+        }
+    }
+}
