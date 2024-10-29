d1895d8eea5e8a66aded8fff389e903a93c2178d
Christopher Jr Riley <chrisjr@cjrriley.com>
2024-10-29 07:03:38 -0400

    Upgrade dependencies

--- Package.swift
+++ Package.swift
@@ -24,8 +24,8 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/swiftlang/swift-docc-plugin.git", from: "1.3.0"),
-        .package(url: "https://github.com/apple/swift-log.git", from: "1.0.0"),
+        .package(url: "https://github.com/swiftlang/swift-docc-plugin.git", from: "1.4.0"),
+        .package(url: "https://github.com/apple/swift-log.git", from: "1.6.0"),
         .package(url: "https://github.com/MasterJ93/SwiftCBOR.git", from: "0.4.0"),
         .package(url: "https://github.com/swiftlang/swift-syntax.git", from: "509.0.0")
     ],
