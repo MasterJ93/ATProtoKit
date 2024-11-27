31a2680cfd4a80d71481fc418283909077b82d72
Andy Lin <andy.lin.2004@outlook.com>
2024-11-27 15:09:51 +0000

    adding convienience delete repost method

--- Sources/ATProtoKit/APIReference/ATProtoBlueskyAPI/RepostRecord/DeleteRepostRecord.swift
+++ Sources/ATProtoKit/APIReference/ATProtoBlueskyAPI/RepostRecord/DeleteRepostRecord.swift
@@ -9,4 +9,14 @@ import Foundation
 
 extension ATProtoBluesky {
 
+    /// Deletes a repost record.
+    ///
+    /// This can also be used to validate if a repost record has been deleted.
+    /// - Parameter record: The record that needs to be deleted.
+    ///
+    /// This can be either the URI of the record, or the full record object itself.
+    public func deleteRepostRecord(_ record: RecordIdentifier) async throws {
+        // in testing, I have found that I can use the preexisting deleteLikeRecord function to delete a repost without any issues.
+        try await deleteLikeRecord(record)
+    }
 }
