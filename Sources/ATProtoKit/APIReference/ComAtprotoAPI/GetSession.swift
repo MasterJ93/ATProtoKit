a6be24738479c961103da493a84e118c3164f0bf
Christopher Jr Riley <chrisjr@cjrriley.com>
2024-12-06 10:40:46 -0500

    Fix documentation for getSession

--- Sources/ATProtoKit/APIReference/ComAtprotoAPI/GetSession.swift
+++ Sources/ATProtoKit/APIReference/ComAtprotoAPI/GetSession.swift
@@ -10,20 +10,25 @@ import Foundation
 extension ATProtoKit {
 
     /// Gets the session information for the user account.
-    ///
+    /// 
     /// It's best to use ``ATProtocolConfiguration`` or another ``SessionConfiguration``-conforming
     /// `class` instead of using this method directly. If you're making a `class` that conforms to
     /// ``SessionConfiguration``, be sure to use this with the method used for getting a session.
-    ///
+    /// 
     /// - Note: According to the AT Protocol specifications: "Get information about the current
     /// auth session. Requires auth."
-    ///
+    /// 
     /// - SeeAlso: This is based on the [`com.atproto.server.getSession`][github] lexicon.
-    ///
+    /// 
     /// [github]: https://github.com/bluesky-social/atproto/blob/main/lexicons/com/atproto/server/getSession.json
     ///
-    /// - Returns: An instance of the session-related information for the user account.
+    /// - Parameters:
+    ///   - accessToken: The access token used for API requests that requests authentication.
+    ///   - pdsURL: The URL of the Personal Data Server (PDS). Optional.
+    ///   Defaults to `https://bsky.social`.
     ///
+    /// - Returns: An instance of the session-related information for the user account.
+    /// 
     /// - Throws: An ``ATProtoError``-conforming error type, depending on the issue. Go to
     /// ``ATAPIError`` and ``ATRequestPrepareError`` for more details.
     public func getSession(
