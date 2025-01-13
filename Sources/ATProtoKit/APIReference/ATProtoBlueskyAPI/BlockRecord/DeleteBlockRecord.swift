a45b7f598828f5dcedd66affeea6290878f5f8bd
Christopher Jr Riley <chrisjr@cjrriley.com>
2025-01-13 17:10:42 -0500

    Add deleteBlockRecord

--- Sources/ATProtoKit/APIReference/ATProtoBlueskyAPI/BlockRecord/DeleteBlockRecord.swift
+++ Sources/ATProtoKit/APIReference/ATProtoBlueskyAPI/BlockRecord/DeleteBlockRecord.swift
@@ -9,4 +9,14 @@ import Foundation
 
 extension ATProtoBluesky {
 
+    /// Deletes a block record.
+    ///
+    /// This can also be used to validate if a block record has been deleted.
+    ///
+    /// - Note: This can be either the URI of the block record, or the full record object itself.
+    ///
+    /// - Parameter record: The block record that needs to be deleted.
+    public func deleteBlockRecord(_ record: RecordIdentifier) async throws {
+        return try await deleteActionRecord(record)
+    }
 }
