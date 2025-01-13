4e196a02faf8c0685b4b97edb83bcb5379110b3f
Christopher Jr Riley <chrisjr@cjrriley.com>
2025-01-13 16:57:36 -0500

    Add deleteFollowRecord

--- Sources/ATProtoKit/APIReference/ATProtoBlueskyAPI/FollowRecord/DeleteFollowRecord.swift
+++ Sources/ATProtoKit/APIReference/ATProtoBlueskyAPI/FollowRecord/DeleteFollowRecord.swift
@@ -9,4 +9,14 @@ import Foundation
 
 extension ATProtoBluesky {
 
+    /// Deletes a follow record.
+    ///
+    /// This can also be used to validate if a follow record has been deleted.
+    ///
+    /// - Note: This can be either the URI of the follow record, or the full record object itself.
+    ///
+    /// - Parameter record: The follow record that needs to be deleted.
+    public func deleteFollowRecord(_ record: RecordIdentifier) async throws {
+        return try await deleteActionRecord(record)
+    }
 }
