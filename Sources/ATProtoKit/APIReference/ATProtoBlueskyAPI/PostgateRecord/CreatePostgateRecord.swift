e45da1f54584fa5c813d12a481fdd1988c61497c
Christopher Jr Riley <chrisjr@cjrriley.com>
2025-01-14 00:01:31 -0500

    Add files for creating, deleting Postgate records

---
+++ Sources/ATProtoKit/APIReference/ATProtoBlueskyAPI/PostgateRecord/CreatePostgateRecord.swift
@@ -0,0 +1,12 @@
+//
+//  CreatePostgateRecord.swift
+//
+//
+//  Created by Christopher Jr Riley on 2025-01-13.
+//
+
+import Foundation
+
+extension ATProtoBluesky {
+
+}
---
+++ Sources/ATProtoKit/APIReference/ATProtoBlueskyAPI/PostgateRecord/DeletePostgateRecord.swift
@@ -0,0 +1,12 @@
+//
+//  DeletePostgateRecord.swift
+//
+//
+//  Created by Christopher Jr Riley on 2025-01-13.
+//
+
+import Foundation
+
+extension ATProtoBluesky {
+
+}
