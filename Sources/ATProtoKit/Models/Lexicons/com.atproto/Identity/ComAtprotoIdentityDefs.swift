77f06837773f80d934a05e0dae2e0aca9e968941
Christopher Jr Riley <chrisjr@cjrriley.com>
2025-02-26 19:25:50 -0500

    Update lexicon models and methods

---
+++ Sources/ATProtoKit/Models/Lexicons/com.atproto/Identity/ComAtprotoIdentityDefs.swift
@@ -0,0 +1,38 @@
+//
+//  ComAtprotoIdentityDefs.swift
+//
+//
+//  Created by Christopher Jr Riley on 2025-02-26.
+//
+
+import Foundation
+
+extension ComAtprotoLexicon.Identity {
+
+    /// A definition model for information about a user account.
+    ///
+    /// - SeeAlso: This is based on the [`com.atproto.identity.defs`][github] lexicon.
+    ///
+    /// [github]: https://github.com/bluesky-social/atproto/blob/main/lexicons/com/atproto/identity/defs.json
+    public struct IdentityInfoDefinition: Sendable, Codable {
+
+        /// The decentralized identifier (DID) of the identity.
+        public let did: String
+
+        /// The handle of the identity.
+        ///
+        /// - Note: According to the AT Protocol specifications: "The validated handle of the account; or 'handle.invalid' if the handle did not bi-directionally match the DID document."
+        public let handle: String
+
+        /// The DID document of the identity.
+        ///
+        /// - Note: According to the AT Protocol specifications: "The complete DID document for the identity."
+        public let didDocument: UnknownType
+
+        enum CodingKeys: String, CodingKey {
+            case did
+            case handle
+            case didDocument = "didDoc"
+        }
+    }
+}
