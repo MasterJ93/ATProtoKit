c604bf2db9791076fe97d9d810146cd1cd149459
Christopher Jr Riley <chrisjr@cjrriley.com>
2025-03-02 16:57:13 -0500

    Refactor updateListRecord
    - Replaced arguments expect for listURI with a "replace" argument. This is
    of type "[UpdatedListRecordField]".
    - Made sure the "replace" argument has a value  before continuing.
    - Check for duplicate case entries.
    - Moved the original record check higher up. This is to get the properties of
    the original record to add them to the new record if there are no changes to a
    specific property.
    - Loop through each item and replace the applicable properties accordingly.

--- Sources/ATProtoKit/APIReference/ATProtoBlueskyAPI/ListRecord/UpdateListRecord.swift
+++ Sources/ATProtoKit/APIReference/ATProtoBlueskyAPI/ListRecord/UpdateListRecord.swift
@@ -12,22 +12,19 @@ extension ATProtoBluesky {
     /// A convenience method to update a list record to the user account in Bluesky.
     /// 
     /// This can be used instead of creating your own method if you wish not to do so.
-    /// 
+    ///
+    /// When you want to erase a property, add the applicable cases and set them to `nil`.
+    ///
+    /// - Important: Don't make duplicates of the same case in the array. This method will
+    /// only take the first instance; the rest will be discarded automatically.
+    ///
     /// - Parameters:
     ///   - listURI: The URI of the post.
-    ///   - name: The name of the list.
-    ///   - listType: The list's type.
-    ///   - description: The list's description. Optional. Defaults to `nil`.
-    ///   - listAvatarImage: The avatar image of the list. Optional. Defaults to `nil`.
-    ///   - labels: An array of labels made by the user. Optional. Defaults to `nil`.
+    ///   - replace: An array of properties where the list record would replace.
     /// - Returns: A strong reference, which contains the newly-created record's URI and CID hash.
     public func updateListRecord(
         listURI: String,
-        name: String,
-        listType: ListType,
-        description: String? = nil,
-        listAvatarImage: ATProtoTools.ImageQuery? = nil,
-        labels: ATUnion.ListLabelsUnion? = nil
+        replace: [UpdatedListRecordField]
     ) async throws -> ComAtprotoLexicon.Repository.StrongReference {
         guard let session else {
             throw ATRequestPrepareError.missingActiveSession
@@ -37,82 +34,156 @@ extension ATProtoBluesky {
             throw ATRequestPrepareError.invalidPDS
         }
 
-        // listPurpose
-        let listPurpose: AppBskyLexicon.Graph.ListPurpose
-        switch listType {
-            case .moderation:
-                listPurpose = .modlist
-            case .curation:
-                listPurpose = .curatelist
-            case .reference:
-                listPurpose = .referencelist
+        if replace.count == 0 {
+            // TODO: Add error case
+            print("The 'replace' argument must contain at least one value.")
         }
 
-        // name
-        // Truncate the number of characters to 64.
-        let nameText = name.truncated(toLength: 64)
+        // Check for duplicates.
+        var seenIdentifiers = Set<String>()
+        var uniqueFields: [UpdatedListRecordField] = []
 
-        // description and descriptionFacets
-        var descriptionText: String? = nil
-        var descriptionFacets: [AppBskyLexicon.RichText.Facet]? = nil
+        for field in replace {
+            // If it reaches the limit, the loop will end early.
+            if uniqueFields.count >= 5 { break }
 
-        if let description = description {
-            // Truncate the number of characters to 300.
-            let truncatedDescriptionText = description.truncated(toLength: 300)
-            descriptionText = truncatedDescriptionText
-
-            let facets = await ATFacetParser.parseFacets(from: truncatedDescriptionText, pdsURL: session.pdsURL ?? "https://bsky.social")
-            descriptionFacets = facets
+            if !seenIdentifiers.contains(field.identifier) {
+                seenIdentifiers.insert(field.identifier)
+                uniqueFields.append(field)
+            }
         }
 
-        // listAvatarImage
-        var postEmbed: ATUnion.PostEmbedUnion? = nil
-        var avatarImage: ComAtprotoLexicon.Repository.UploadBlobOutput? = nil
-        if let listAvatarImage = listAvatarImage {
-            postEmbed = try await uploadImages(
-                [listAvatarImage],
-                pdsURL: sessionURL,
-                accessToken: session.accessToken
-            )
+        guard let record = try await atProtoKitInstance.getPosts([listURI]).posts.first,
+              let list = record.record.getRecord(ofType: AppBskyLexicon.Graph.ListRecord.self) else {
+            throw ATProtoBlueskyError.postNotFound(message: "Post (\(listURI)) not found.")
+        }
 
-            switch postEmbed {
-                case .images(let imagesDefinition):
-                    let avatarImageContainer = imagesDefinition
-                    avatarImage = avatarImageContainer.images[0].imageBlob
-                default:
-                    break
+        // Make placeholder variables and hold the current record's values into them.
+        // These variables will be part of the new record.
+        var newListType: AppBskyLexicon.Graph.ListPurpose = list.purpose
+        var newName: String = list.name
+        var newDescription: String? = list.description
+        var newDescriptionFacets: [AppBskyLexicon.RichText.Facet]? = list.descriptionFacets
+        var newListAvatarImage: ComAtprotoLexicon.Repository.UploadBlobOutput? = list.avatarImageBlob
+        var newLabels: ATUnion.ListLabelsUnion? = list.labels
+
+        // Loop through each item of the array.
+        for uniqueField in uniqueFields {
+            // For each field, replace the values above with the applicable case.
+            switch uniqueField {
+                case .purpose(with: let listTypeField):
+                    // Check which case was selected.
+                    switch listTypeField {
+                        case .moderation:
+                            newListType = .modlist
+                        case .curation:
+                            newListType = .curatelist
+                        case .reference:
+                            newListType = .referencelist
+                    }
+                case .name(with: let nameField):
+                    // Truncate the number of characters to 64.
+                    newName = nameField.truncated(toLength: 64)
+                case .description(with: let descriptionField):
+                    if let descriptionField = descriptionField {
+                        let truncatedDescription = descriptionField.truncated(toLength: 300)
+                        let facets = await ATFacetParser.parseFacets(from: truncatedDescription, pdsURL: session.pdsURL ?? "https://bsky.social")
+
+                        newDescription = truncatedDescription
+                        newDescriptionFacets = facets
+                    } else if descriptionField == nil || descriptionField?.count == 0 {
+                        newDescription = nil
+                        newDescriptionFacets = nil
+                    }
+
+                case .listAvatarImage(with: let listAvatarImageField):
+                    // Check if the field is nil. If so, set the listAvatarImage variable to nil and break out of the case early.
+                    if listAvatarImageField == nil {
+                        newListAvatarImage = nil
+
+                        break
+                    }
+
+                    if let listAvatarImageField = listAvatarImageField {
+                        let postEmbed = try await uploadImages(
+                            [listAvatarImageField],
+                            pdsURL: sessionURL,
+                            accessToken: session.accessToken
+                        )
+
+                        switch postEmbed {
+                            case .images(let imagesDefinition):
+                                newListAvatarImage = imagesDefinition.images[0].imageBlob
+                            default:
+                                break
+                        }
+                    }
+                case .labels(with: let labelsField):
+                    newLabels = labelsField
             }
         }
 
         let listRecord = AppBskyLexicon.Graph.ListRecord(
-            purpose: listPurpose,
-            name: nameText,
-            description: descriptionText,
-            descriptionFacets: descriptionFacets,
-            avatarImageBlob: avatarImage,
-            labels: labels,
+            purpose: newListType,
+            name: newName,
+            description: newDescription,
+            descriptionFacets: newDescriptionFacets,
+            avatarImageBlob: newListAvatarImage,
+            labels: newLabels,
             createdAt: Date()
         )
 
         do {
-            let uri = try ATProtoTools().parseURI(listURI)
-
-            guard try await atProtoKitInstance.getRepositoryRecord(
-                from: uri.repository,
-                collection: uri.collection,
-                recordKey: uri.recordKey
-            ).value != nil else {
-                throw ATProtoBlueskyError.postNotFound(message: "List record (\(listURI)) not found.")
-            }
+            let recordKey = try ATProtoTools().parseURI(listURI).recordKey
 
             return try await atProtoKitInstance.putRecord(
                 repository: session.sessionDID,
                 collection: "app.bsky.graph.list",
-                recordKey: uri.recordKey,
+                recordKey: recordKey,
                 record: UnknownType.record(listRecord)
             )
         } catch {
             throw error
+        }
+    }
+
+    /// An enumeration for updating the list record.
+    public enum UpdatedListRecordField {
+
+        /// The list's type.
+        ///
+        /// - Parameter with: The object to update the record with.
+        case purpose(with: ListType)
+
+        /// The name of the list.
+        ///
+        /// - Parameter with: The object to update the record with.
+        case name(with: String)
+
+        /// The list's description.
+        ///
+        /// - Parameter with: The object to update the record with. Optional. Defaults to `nil`.
+        case description(with: String? = nil)
+
+        /// The avatar image of the list.
+        ///
+        /// - Parameter with: The object to update the record with. Optional. Defaults to `nil`.
+        case listAvatarImage(with: ATProtoTools.ImageQuery? = nil)
+
+        /// An array of labels made by the user.
+        ///
+        /// - Parameter with: The object to update the record with. Optional. Defaults to `nil`.
+        case labels(with: ATUnion.ListLabelsUnion? = nil)
+
+        /// Identifies the field.
+        var identifier: String {
+            switch self {
+                case .purpose: return "purpose"
+                case .name: return "name"
+                case .description: return "description"
+                case .listAvatarImage: return "listAvatarImage"
+                case .labels: return "labels"
+            }
         }
     }
 }
