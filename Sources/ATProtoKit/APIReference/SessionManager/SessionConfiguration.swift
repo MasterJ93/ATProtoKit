db3dd8d24d07b392a21caf8ed9e32d69f1936642
Christopher Jr Riley <chrisjr@cjrriley.com>
2025-04-04 01:48:13 -0400

    Update SessionConfiguration
    - Added a property for a UUID.
    - Removed session property.
    - Added properties for creating an account, creating
    a session, getting a session, and deleting a session.

--- Sources/ATProtoKit/APIReference/SessionManager/SessionConfiguration.swift
+++ Sources/ATProtoKit/APIReference/SessionManager/SessionConfiguration.swift
@@ -8,13 +8,19 @@
 import Foundation
 
 /// Defines the requirements for session configurations within ATProtoKit.
-public protocol SessionConfiguration {
-
-    /// The user's unique handle used for authentication purposes.
-    var handle: String { get }
-
-    /// The app password associated with the user's account, used for authentication.
-    var password: String { get }
+///
+/// This protocol contains default implementations for the methods used. You can choose
+/// to use those implementations, or create your own.
+///
+/// When implementing this protocol onto your `class`, you should keep in mind of a few things:
+/// - `class`es conforming to `SessionConfiguration` must be `final` and conforming
+/// to `Sendable`.
+/// - You should use `UserSessionRegistry` to manage `UserSession` instances.
+/// - When deleting a session, make sure that `deleteSession` removes the instance
+/// from `UserSessionRegistry`.
+/// - Accessing the access token, refresh token, or password requires
+/// using `KeychainProtocol`.
+public protocol SessionConfiguration: AnyObject, Sendable {
 
     /// The base URL of the Personal Data Server (PDS) with which the AT Protocol interacts.
     ///
@@ -25,10 +31,50 @@ public protocol SessionConfiguration {
     /// An instance of `URLSessionConfiguration`.
     var configuration: URLSessionConfiguration { get }
 
-    /// The object attached to the configuration class that holds the session. Optional.
+    /// A `UUID` object specific to the `UserSession` instance.
     ///
-    /// This also includes things such as retry limits and logging.
-    var session: UserSession? { get }
+    /// This is used to look for the `UserSession` instance within `UserSessionRegistry`.
+    var instanceUUID: UUID { get }
+
+    /// Creates an a new account for the user.
+    ///
+    /// - Note: `plcOp` may be updated when full account migration is implemented.
+    ///
+    /// - Bug: `plcOp` is currently broken: there's nothing that can be used for this at the
+    /// moment while Bluesky continues to work on account migration. Until everything settles
+    /// and they have a concrete example of what to do, don't use it. In the meantime, leave it
+    /// at `nil`.
+    ///
+    /// - Parameters:
+    ///   - email: The email of the user. Optional
+    ///   - handle: The handle the user wishes to use.
+    ///   - existingDID: A decentralized identifier (DID) that has existed before and will be
+    ///   used to be imported to the new account. Optional.
+    ///   - inviteCode: The invite code for the user. Optional.
+    ///   - verificationCode: A verification code.
+    ///   - verificationPhone: A code that has come from a text message in the user's
+    ///   phone. Optional.
+    ///   - password: The password the user will use for the account. Optional.
+    ///   - recoveryKey: DID PLC rotation key (aka, recovery key) to be included in PLC
+    ///   creation operation. Optional.
+    ///   - plcOperation: A signed DID PLC operation to be submitted as part of importing an
+    ///   existing account to this instance. Optional.
+    /// - Returns: An instance of an authenticated user session within the AT Protocol. It may also
+    /// have logging information, as well as the URL of the Personal Data Server (PDS).
+    ///
+    /// - Throws: An ``ATProtoError``-conforming error type, depending on the issue. Go to
+    /// ``ATAPIError`` and ``ATRequestPrepareError`` for more details.
+    func createAccount(
+        email: String?,
+        handle: String,
+        existingDID: String?,
+        inviteCode: String?,
+        verificationCode: String?,
+        verificationPhone: String?,
+        password: String?,
+        recoveryKey: String?,
+        plcOperation: UnknownType?
+    ) async throws
 
     /// Attempts to authenticate with the PDS using the `handle` and `appPassword`.
     ///
@@ -41,5 +87,37 @@ public protocol SessionConfiguration {
     /// Two-Factor Authentication. Optional.
     ///
     /// - Throws: An error if there are issues creating the request or communicating with the PDS.
-    func authenticate(authenticationFactorToken: String?) async throws
+    func authenticate() async throws
+
+    /// Fetches an existing session using an access token.
+    ///
+    /// If the access token is invalid, then a new one will be created, either by refeshing a
+    /// session, or by re-authenticating.
+    ///
+    /// - Throws: An ``ATProtoError``-conforming error type, depending on the issue. Go to
+    /// ``ATAPIError`` and ``ATRequestPrepareError`` for more details.
+    func getSession() async throws
+
+    /// Refreshes the user's session using a refresh token.
+    ///
+    /// If the refresh token is invalid, the method will re-authenticate and try again.
+    ///
+    /// - Note: If the method throws an error saying that an authentication token is required,
+    /// re-trying the method with the `authenticationFactorToken` argument filled should
+    /// solve the issue.
+    ///
+    /// When the method completes, ``ATProtocolConfiguration/session`` will be updated with a
+    /// new instance of an authenticated user session within the AT Protocol. It may also have
+    /// logging information, as well as the URL of the Personal Data Server (PDS).
+    ///
+    /// - Returns: Information of the user account's new session.
+    /// - Throws: An ``ATProtoError``-conforming error type, depending on the issue. Go to
+    /// ``ATAPIError`` and ``ATRequestPrepareError`` for more details.
+    func refreshSession() async throws
+
+    /// Deletes the user session.
+    ///
+    /// - Throws: An ``ATProtoError``-conforming error type, depending on the issue. Go to
+    /// ``ATAPIError`` and ``ATRequestPrepareError`` for more details.
+    func deleteSession() async throws
 }
