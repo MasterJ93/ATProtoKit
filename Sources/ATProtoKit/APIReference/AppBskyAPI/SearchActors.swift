ebd5f2602d166c584f59899bb0169c2b0d7c5524
Christopher Jr Riley <chrisjr@cjrriley.com>
2025-05-11 02:10:45 -0400

    Update searchActors()
    - Removed the "shouldAuthenticate" method, as the Bluesky
    team is preparing to change the method to only allow for
    authenticated requests.
    - Changed the code to only accept authenticated requests.

--- Sources/ATProtoKit/APIReference/AppBskyAPI/SearchActors.swift
+++ Sources/ATProtoKit/APIReference/AppBskyAPI/SearchActors.swift
@@ -16,12 +16,6 @@ extension ATProtoKit {
     /// suggestion (where it returns results based on a partial term instead of the exact term),
     /// a different method is needed.
     ///
-    /// - Bug: According to the AT Protocol specifications, this API call does not require
-    /// authentication. However, there's an issue where it asks for authentication if there's
-    /// no `accessToken`. It's unknown whether this is an issue on the AT Protocol's end or
-    /// `AKProtoKit`'s end. For now, use the `shouldAuthenticate` parameter when
-    /// using this method.
-    ///
     /// - Note: According to the AT Protocol specifications: "Find actors (profiles) matching
     /// search criteria. Does not require auth."
     ///
@@ -44,19 +38,17 @@ extension ATProtoKit {
     public func searchActors(
         matching query: String,
         limit: Int? = 25,
-        cursor: String? = nil,
-        shouldAuthenticate: Bool = true
+        cursor: String? = nil
     ) async throws -> AppBskyLexicon.Actor.SearchActorsOutput {
-        let authorizationValue = await prepareAuthorizationValue(
-            shouldAuthenticate: shouldAuthenticate
-        )
-
-        guard self.pdsURL != "" else {
-            throw ATRequestPrepareError.emptyPDSURL
+        guard let session = try await self.getUserSession(),
+              let keychain = sessionConfiguration?.keychainProtocol else {
+            throw ATRequestPrepareError.missingActiveSession
         }
 
-        guard let sessionURL = authorizationValue != nil ? try await self.getUserSession()?.serviceEndpoint.absoluteString : self.pdsURL,
-              let requestURL = URL(string: "\(sessionURL)/xrpc/app.bsky.actor.searchActors") else {
+        let accessToken = try await keychain.retrieveAccessToken()
+        let sessionURL = session.serviceEndpoint.absoluteString
+
+        guard let requestURL = URL(string: "\(sessionURL)/xrpc/app.bsky.actor.searchActors") else {
             throw ATRequestPrepareError.invalidRequestURL
         }
 
@@ -84,7 +76,7 @@ extension ATProtoKit {
                 forRequest: queryURL,
                 andMethod: .get,
                 acceptValue: "application/json",
-                authorizationValue: authorizationValue
+                authorizationValue: "Bearer \(accessToken)"
             )
             let response = try await APIClientService.shared.sendRequest(
                 request,
