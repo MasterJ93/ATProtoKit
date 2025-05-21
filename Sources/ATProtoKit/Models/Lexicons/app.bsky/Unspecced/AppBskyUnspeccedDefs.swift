3fe5fe1722e8dbe292995715170ea97a1d96262f
Christopher Jr Riley <chrisjr@cjrriley.com>
2025-05-20 23:12:32 -0400

    Fix issue where the Date object wasn't encoded and decoded properly

--- Sources/ATProtoKit/Models/Lexicons/app.bsky/Unspecced/AppBskyUnspeccedDefs.swift
+++ Sources/ATProtoKit/Models/Lexicons/app.bsky/Unspecced/AppBskyUnspeccedDefs.swift
@@ -116,6 +116,43 @@ extension AppBskyLexicon.Unspecced {
         /// An array of decentralized identifiers (DIDs) related to the topic.
         public let dids: [String]
 
+        public init(topic: String, displayName: String, link: String, startedAt: Date, postCount: Int, status: Status?, category: String?, dids: [String]) {
+            self.topic = topic
+            self.displayName = displayName
+            self.link = link
+            self.startedAt = startedAt
+            self.postCount = postCount
+            self.status = status
+            self.category = category
+            self.dids = dids
+        }
+
+        public init(from decoder: any Decoder) throws {
+            let container = try decoder.container(keyedBy: CodingKeys.self)
+
+            self.topic = try container.decode(String.self, forKey: .topic)
+            self.displayName = try container.decode(String.self, forKey: .displayName)
+            self.link = try container.decode(String.self, forKey: .link)
+            self.startedAt = try container.decodeDate(forKey: .startedAt)
+            self.postCount = try container.decode(Int.self, forKey: .postCount)
+            self.status = try container.decodeIfPresent(AppBskyLexicon.Unspecced.SkeletonTrendDefinition.Status.self, forKey: .status)
+            self.category = try container.decodeIfPresent(String.self, forKey: .category)
+            self.dids = try container.decode([String].self, forKey: .dids)
+        }
+
+        public func encode(to encoder: any Encoder) throws {
+            var container = encoder.container(keyedBy: CodingKeys.self)
+
+            try container.encode(self.topic, forKey: .topic)
+            try container.encode(self.displayName, forKey: .displayName)
+            try container.encode(self.link, forKey: .link)
+            try container.encodeDate(self.startedAt, forKey: .startedAt)
+            try container.encode(self.postCount, forKey: .postCount)
+            try container.encodeIfPresent(self.status, forKey: .status)
+            try container.encodeIfPresent(self.category, forKey: .category)
+            try container.encode(self.dids, forKey: .dids)
+        }
+
         enum CodingKeys: CodingKey {
             case topic
             case displayName
@@ -146,7 +183,7 @@ extension AppBskyLexicon.Unspecced {
     /// [github]: https://github.com/bluesky-social/atproto/blob/main/lexicons/app/bsky/unspecced/defs.json
     public struct TrendViewDefinition: Sendable, Codable {
 
-        ///
+        /// A shorthand, lowercased version of the topic name.
         public let topic: String
 
         /// The display name of the topic.
@@ -169,6 +206,55 @@ extension AppBskyLexicon.Unspecced {
 
         /// An array of profiles related to the topic.
         public let actors: [AppBskyLexicon.Actor.ProfileViewBasicDefinition]
+
+        public init(topic: String, displayName: String, link: String, startedAt: Date, postCount: Int, status: Status?, category: String?,
+            actors: [AppBskyLexicon.Actor.ProfileViewBasicDefinition]) {
+            self.topic = topic
+            self.displayName = displayName
+            self.link = link
+            self.startedAt = startedAt
+            self.postCount = postCount
+            self.status = status
+            self.category = category
+            self.actors = actors
+        }
+
+        enum CodingKeys: CodingKey {
+            case topic
+            case displayName
+            case link
+            case startedAt
+            case postCount
+            case status
+            case category
+            case actors
+        }
+
+        public init(from decoder: any Decoder) throws {
+            let container = try decoder.container(keyedBy: CodingKeys.self)
+
+            self.topic = try container.decode(String.self, forKey: .topic)
+            self.displayName = try container.decode(String.self, forKey: .displayName)
+            self.link = try container.decode(String.self, forKey: .link)
+            self.startedAt = try container.decodeDate(forKey: .startedAt)
+            self.postCount = try container.decode(Int.self, forKey: .postCount)
+            self.status = try container.decodeIfPresent(AppBskyLexicon.Unspecced.TrendViewDefinition.Status.self, forKey: .status)
+            self.category = try container.decodeIfPresent(String.self, forKey: .category)
+            self.actors = try container.decode([AppBskyLexicon.Actor.ProfileViewBasicDefinition].self, forKey: .actors)
+        }
+
+        public func encode(to encoder: any Encoder) throws {
+            var container = encoder.container(keyedBy: CodingKeys.self)
+
+            try container.encode(self.topic, forKey: .topic)
+            try container.encode(self.displayName, forKey: .displayName)
+            try container.encode(self.link, forKey: .link)
+            try container.encodeDate(self.startedAt, forKey: .startedAt)
+            try container.encode(self.postCount, forKey: .postCount)
+            try container.encodeIfPresent(self.status, forKey: .status)
+            try container.encodeIfPresent(self.category, forKey: .category)
+            try container.encode(self.actors, forKey: .actors)
+        }
 
         // Enums
         /// The status of the topic.
