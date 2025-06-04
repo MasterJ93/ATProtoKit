3eed727924f72b3f75239cf78d9fd1107e27826a
Christopher Jr Riley <chrisjr@cjrriley.com>
2025-06-04 01:38:51 -0400

    Update documentation in APIHostname
    Added the links directly onto the documentation
    to be able to quickly glance over which one it is
    without looking directly at the code.

--- Sources/ATProtoKit/Utilities/APIHostname.swift
+++ Sources/ATProtoKit/Utilities/APIHostname.swift
@@ -10,18 +10,18 @@ import Foundation
 /// A list of hostnames used for the AT Protocol.
 public enum APIHostname {
 
-    /// The hostname for Bluesky's Relay.
+    /// The hostname for Bluesky's Relay ("https://bsky.network").
     public static let relay: String = "https://bsky.network"
 
-    /// The hostname for Bluesky's proxy entryway.
+    /// The hostname for Bluesky's proxy entryway ("https://bsky.social").
     public static let entryway: String = "https://bsky.social"
 
-    /// The hostname for Bluesky's AppView.
+    /// The hostname for Bluesky's AppView ("https://public.api.bsky.app").
     public static let bskyAppView: String = "https://public.api.bsky.app"
 
-    /// The hostname for Bluesky's chat API.
+    /// The hostname for Bluesky's chat API ("https://api.bsky.chat").
     public static let bskyChat: String = "https://api.bsky.chat"
 
-    /// The hostname for the Ozone moderation API.
+    /// The hostname for the Ozone moderation API ("https://mod.bsky.app").
     public static let moderation: String = "https://mod.bsky.app"
 }
